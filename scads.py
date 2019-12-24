da2931a4688a16f1a791a03a2be0c03c4f9d3f89
Jeffrey Zhu <jeffrey_zhu@brown.edu>
2019-12-24 14:02:51 -0500

    update Scads docstrings/comments

--- scads.py
+++ scads.py
@@ -2,7 +2,8 @@ import numpy as np
 
 
 class Scads:
-    """ scads class
+    """
+    Scads class
     Structured Collections of Annotated Data Sets (SCADS)
     """
     def __init__(self):
@@ -10,21 +11,24 @@ class Scads:
         self.datasets = []  # list of our datasets
 
     def get_datasets(self):
-        '''
-        update list of datasets
+        """
+        Update list of datasets.
         :return:
-        '''
-    def get_image(self, concept):
-        '''
-        get all images for a concept
+        """
+
+    def get_images(self, concept):
+        """
+        Jeff: Is the concept (here and in get_neighbors) just a wordId?
+        Jeff: Also, is this returning the list of paths to images, or the actual numpy arrays?
+
+        Get all images for a concept.
         :param concept:
         :return:
-        '''
-
+        """
 
     def get_neighbors(self, concept):
-        '''
-        get the neighbours with the type of relationship
+        """
+        Get the neighbors of a concept with the type of relationship.
         :param concept:
-        :return:
-        '''
+        :return: List of (Concept, Relationship) tuples
+        """
