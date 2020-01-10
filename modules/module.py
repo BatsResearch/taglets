8f7fb36c1682dc8bccf9ad7ec2799b46733a5c26
Jeffrey Zhu <jeffrey_zhu@brown.edu>
2020-01-10 12:22:44 -0500

    move training from executor to module

--- modules/module.py
+++ modules/module.py
@@ -9,18 +9,19 @@ class BaseModule:
     """
     def __init__(self, task):
         """
+        Jeff: The list of taglets will look different based future subclasses of this BaseModule
         :param task: The task for the module to consume
         """
         self.task = task
+        # Currently hardcoded
+        self.taglets = [ResnetTaglet(), LogisticRegressionTaglet(), PrototypeTaglet()]
+
+    def train_taglets(self, labeled_images, batch_size=64, use_gpu=True):
+        for taglet in self.taglets:
+            taglet.train(labeled_images)
 
     def get_taglets(self):
         """
-        Jeff: We could also have a method to load taglets before returning them here.
         :return: List of taglets
         """
-        # for now this is hard-coded
-        taglets = [ResnetTaglet(), LogisticRegressionTaglet(), PrototypeTaglet()]
-        labeled_data = None     # TODO
-        for taglet in taglets:
-            taglet.train(labeled_data)
-        return taglets
+        return self.taglets
