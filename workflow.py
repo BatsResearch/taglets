354c43c5d5589d5f901c760d5cffe1b3f4e1e187
Jeffrey Zhu <jeffrey_zhu@brown.edu>
2020-01-10 14:15:44 -0500

    add scads_example to workflow.py

--- workflow.py
+++ workflow.py
@@ -1,6 +1,22 @@
+from scads import Scads
+
 from modules.module import BaseModule
 from taglet_executer import TagletExecuter
 from task import MNIST
+
+
+def scads_example():
+    Scads.open()                                    # Start the session
+    node = Scads.get_node('/c/ab/агыруа/n')         # Get a node
+    print(node.get_datasets())                      # Get list of dataset names
+    print(node.get_images())                        # Get list of image paths
+    outgoing_edges = node.get_neighbors()           # Get node neighbors
+    print(outgoing_edges[0].relationship)           # Get the type of relationship
+    neighboring_node = outgoing_edges[0].end_node   # Get the neighboring node
+
+    # The neighbor is also a ScadsNode
+    print(neighboring_node.get_datasets(), neighboring_node.get_images(), neighboring_node.get_neighbors)
+    Scads.close()                                   # Close the session
 
 
 def workflow():
