ec000cb9fc293d79f5058a8812f74967ee451a0e
topwasu <wasu.piri@gmail.com>
2020-01-11 21:34:47 +0700

    Implement train and execute methods for DL-based taglets

--- taglet.py
+++ taglet.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 import torchvision.models as models
 
 
@@ -25,11 +26,55 @@ class ResnetTaglet(Taglet):
 
         self.model = models.resnet18(pretrained=self.pretrained)
 
-    def train(self, labeled_data, batch_size, use_gpu):
-        print('train model on labeled data')
+    def train(self, images, labels, lr=1e-3, num_epochs=100, batch_size=64, use_gpu=True):
+        num_images = images.shape[0]
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)
+        classification_criterion = torch.nn.CrossEntropyLoss()
+        
+        # Top: not sure if this is the most efficient way of doing it
+        self.model = self.model.train()
+        if use_gpu:
+            self.model = self.model.cuda()
+        else:
+            self.model = self.model.cpu()
+        
+        for epoch in range(num_epochs):
+            perm = torch.randperm(num_images)
+            for i in range(0, num_images, batch_size):
+                optimizer.zero_grad()
+                
+                ind = perm[i: i+batch_size]
+                batch_images = images[ind]
+                batch_labels = labels[ind]
+                
+                if use_gpu:
+                    batch_images = batch_images.cuda()
+                    batch_labels = batch_labels.cuda()
+                
+                predicted_labels = self.model(batch_images)
+                loss = classification_criterion(predicted_labels, batch_labels)
+                
+                loss.backward()
+                optimizer.step()
 
-    def execute(self, unlabeled_images, use_gpu=True):
-        print('execute vote on unlabeled data')
+    def execute(self, unlabeled_images, batch_size=64, use_gpu=True):
+        num_images = unlabeled_images.shape[0]
+        self.model = self.model.eval()
+        if use_gpu:
+            self.model = self.model.cuda()
+        else:
+            self.model = self.model.cpu()
+        list_predicted_labels = []
+        for i in range(0, num_images, batch_size):
+            batch_images = unlabeled_images[i: i+batch_size]
+    
+            if use_gpu:
+                batch_images = batch_images.cuda()
+    
+            predicted_labels = self.model(batch_images)
+            list_predicted_labels.append(predicted_labels.cpu().detach().numpy())
+        all_predicted_labels = np.concatenate(list_predicted_labels)
+        return all_predicted_labels
 
 
 class LogisticRegressionTaglet(Taglet):
@@ -37,13 +82,57 @@ class LogisticRegressionTaglet(Taglet):
         super().__init__()
         self.pretrained = True
 
-        # self.model = one layer NN
+        # self.model = torch.nn.Linear ...
 
-    def train(self, labeled_data, batch_size, use_gpu):
-        print('train model on labeled data')
+    def train(self, images, labels, lr=1e-3, num_epochs=100, batch_size=64, use_gpu=True):
+        num_images = images.shape[0]
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)
+        classification_criterion = torch.nn.CrossEntropyLoss()
+    
+        # Top: not sure if this is the most efficient way of doing it
+        self.model = self.model.train()
+        if use_gpu:
+            self.model = self.model.cuda()
+        else:
+            self.model = self.model.cpu()
+    
+        for epoch in range(num_epochs):
+            perm = torch.randperm(num_images)
+            for i in range(0, num_images, batch_size):
+                optimizer.zero_grad()
+            
+                ind = perm[i: i + batch_size]
+                batch_images = images[ind]
+                batch_labels = labels[ind]
+            
+                if use_gpu:
+                    batch_images = batch_images.cuda()
+                    batch_labels = batch_labels.cuda()
+            
+                predicted_labels = self.model(batch_images)
+                loss = classification_criterion(predicted_labels, batch_labels)
+            
+                loss.backward()
+                optimizer.step()
 
-    def execute(self, unlabeled_images, use_gpu=True):
-        print('execute vote on unlabeled data')
+    def execute(self, unlabeled_images, batch_size=64, use_gpu=True):
+        num_images = unlabeled_images.shape[0]
+        self.model = self.model.eval()
+        if use_gpu:
+            self.model = self.model.cuda()
+        else:
+            self.model = self.model.cpu()
+        list_predicted_labels = []
+        for i in range(0, num_images, batch_size):
+            batch_images = unlabeled_images[i: i + batch_size]
+        
+            if use_gpu:
+                batch_images = batch_images.cuda()
+        
+            predicted_labels = self.model(batch_images)
+            list_predicted_labels.append(predicted_labels.cpu().detach().numpy())
+        all_predicted_labels = np.concatenate(list_predicted_labels)
+        return all_predicted_labels
 
 
 class PrototypeTaglet(Taglet):
@@ -53,8 +142,52 @@ class PrototypeTaglet(Taglet):
 
         # self.model = Peilin will take care of this
 
-    def train(self, labeled_data, batch_size, use_gpu):
-        print('train model on labeled data')
+    def train(self, images, labels, lr=1e-3, num_epochs=100, batch_size=64, use_gpu=True):
+        num_images = images.shape[0]
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)
+        classification_criterion = torch.nn.CrossEntropyLoss()
+    
+        # Top: not sure if this is the most efficient way of doing it
+        self.model = self.model.train()
+        if use_gpu:
+            self.model = self.model.cuda()
+        else:
+            self.model = self.model.cpu()
+    
+        for epoch in range(num_epochs):
+            perm = torch.randperm(num_images)
+            for i in range(0, num_images, batch_size):
+                optimizer.zero_grad()
+            
+                ind = perm[i: i + batch_size]
+                batch_images = images[ind]
+                batch_labels = labels[ind]
+            
+                if use_gpu:
+                    batch_images = batch_images.cuda()
+                    batch_labels = batch_labels.cuda()
+            
+                predicted_labels = self.model(batch_images)
+                loss = classification_criterion(predicted_labels, batch_labels)
+            
+                loss.backward()
+                optimizer.step()
 
-    def execute(self, unlabeled_images, use_gpu=True):
-        print('execute vote on unlabeled data')
+    def execute(self, unlabeled_images, batch_size=64, use_gpu=True):
+        num_images = unlabeled_images.shape[0]
+        self.model = self.model.eval()
+        if use_gpu:
+            self.model = self.model.cuda()
+        else:
+            self.model = self.model.cpu()
+        list_predicted_labels = []
+        for i in range(0, num_images, batch_size):
+            batch_images = unlabeled_images[i: i + batch_size]
+        
+            if use_gpu:
+                batch_images = batch_images.cuda()
+        
+            predicted_labels = self.model(batch_images)
+            list_predicted_labels.append(predicted_labels.cpu().detach().numpy())
+        all_predicted_labels = np.concatenate(list_predicted_labels)
+        return all_predicted_labels
