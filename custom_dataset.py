212b8d1480be1e9a171e01bb0d163ea9914f1156
elahehraisi <maman>
2020-01-18 21:03:23 -0500

    custom dataset to laod a dataset from folder

--- custom_dataset.py
+++ custom_dataset.py
@@ -1,58 +1,30 @@
-from scads import Scads, add_conceptnet, add_datasets
-from modules.module import BaseModule
-from taglet_executer import TagletExecuter
-from task import Task
-from JPL_interface import JPL
+from torch.utils.data import Dataset
+from PIL import Image
+import os
+import torch
 
 
+class CustomDataSet(Dataset):
+    def __init__(self, root, images, labels, transform, num_channels):
+        self.root = root
+        self.images = images
+        self.labels = labels
+        self.transform = transform
+        self.num_channels = num_channels
 
-def create_scads():
-    add_conceptnet()
-    add_datasets()
+    def __getitem__(self, index):
+        img = os.path.join(self.root,self.images[index])
+        img = Image.open(img)
+        if self.num_channels == 3:
+            img = img.convert('RGB')
 
+        if self.transform is not None:
+            img = self.transform(img)
 
-def scads_example():
-    Scads.open()                                    # Start the session
-    node = Scads.get_node('/c/ab/агыруа/n')         # Get a node
-    print(node.get_datasets())                      # Get list of dataset names
-    print(node.get_images())                        # Get list of image paths
-    outgoing_edges = node.get_neighbors()           # Get node neighbors
-    print(outgoing_edges[0].relationship)           # Get the type of relationship
-    neighboring_node = outgoing_edges[0].end_node   # Get the neighboring node
-
-    # The neighbor is also a ScadsNode
-    print(neighboring_node.get_datasets(), neighboring_node.get_images(), neighboring_node.get_neighbors())
-    Scads.close()                                   # Close the session
-
-
-def workflow():
-    JPL_API = JPL()
-    tasks = JPL_API.get_available_tasks()
-    task_name = tasks[1]    # Problem_test_image_classification
-    task_metadata = JPL_API.get_task_metadata(task_name)
-    current_task = Task(task_metadata)
-
-    JPL_API.create_session(task_name)
-    session_status = JPL_API.get_session_status()
-    current_dataset = session_status['current_dataset']
-    current_task.classes = current_dataset['classes']
-    current_task.unlabeled_images = current_dataset.data_url
-    current_task.labeled_images = JPL_API.get_seed_labels()
-
-    MNIST_module = BaseModule(task=current_task)
-    MNIST_module.train_taglets(current_task.labeled_images)
-    taglets = MNIST_module.get_taglets()
-    taglet_executer = TagletExecuter(taglets)
-    label_matrix = taglet_executer.execute(current_task.unlabeled_images)
-    # soft_labels = LabelModel.annotate(label_matrix)
-    # end_model = end_model(soft_labels, self.unlabeled_images)
-    # [test_predictions] = end_model.prediction(end_model, self.test_images)
+        label = torch.tensor(int(self.labels[index]))
+        # return img, self.labels[index], index
+        return img, label, index
 
 
-def main():
-    # create_scads()
-    # scads_example()
-    workflow()
-
-if __name__ == "__main__":
-    main()
+    def __len__(self):
+        return len(self.images)
