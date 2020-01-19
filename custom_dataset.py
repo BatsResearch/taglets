c4139111f1d86b016cd2c43806f9ca47fb741fcd
elahehraisi <maman>
2020-01-18 21:01:10 -0500

    added fine-tune taglet

---
+++ custom_dataset.py
@@ -0,0 +1,58 @@
+from scads import Scads, add_conceptnet, add_datasets
+from modules.module import BaseModule
+from taglet_executer import TagletExecuter
+from task import Task
+from JPL_interface import JPL
+
+
+
+def create_scads():
+    add_conceptnet()
+    add_datasets()
+
+
+def scads_example():
+    Scads.open()                                    # Start the session
+    node = Scads.get_node('/c/ab/агыруа/n')         # Get a node
+    print(node.get_datasets())                      # Get list of dataset names
+    print(node.get_images())                        # Get list of image paths
+    outgoing_edges = node.get_neighbors()           # Get node neighbors
+    print(outgoing_edges[0].relationship)           # Get the type of relationship
+    neighboring_node = outgoing_edges[0].end_node   # Get the neighboring node
+
+    # The neighbor is also a ScadsNode
+    print(neighboring_node.get_datasets(), neighboring_node.get_images(), neighboring_node.get_neighbors())
+    Scads.close()                                   # Close the session
+
+
+def workflow():
+    JPL_API = JPL()
+    tasks = JPL_API.get_available_tasks()
+    task_name = tasks[1]    # Problem_test_image_classification
+    task_metadata = JPL_API.get_task_metadata(task_name)
+    current_task = Task(task_metadata)
+
+    JPL_API.create_session(task_name)
+    session_status = JPL_API.get_session_status()
+    current_dataset = session_status['current_dataset']
+    current_task.classes = current_dataset['classes']
+    current_task.unlabeled_images = current_dataset.data_url
+    current_task.labeled_images = JPL_API.get_seed_labels()
+
+    MNIST_module = BaseModule(task=current_task)
+    MNIST_module.train_taglets(current_task.labeled_images)
+    taglets = MNIST_module.get_taglets()
+    taglet_executer = TagletExecuter(taglets)
+    label_matrix = taglet_executer.execute(current_task.unlabeled_images)
+    # soft_labels = LabelModel.annotate(label_matrix)
+    # end_model = end_model(soft_labels, self.unlabeled_images)
+    # [test_predictions] = end_model.prediction(end_model, self.test_images)
+
+
+def main():
+    # create_scads()
+    # scads_example()
+    workflow()
+
+if __name__ == "__main__":
+    main()
--- taglet.py
+++ taglet.py
@@ -1,23 +1,249 @@
 import numpy as np
 import torch
 import torchvision.models as models
+from numpy import inf
+from abc import abstractmethod
+import random
+import copy
+import torchvision.models as models
+import os
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+import torch
+from custom_dataset import CustomDataSet
+from torch.utils.data.sampler import SubsetRandomSampler
+from torch.utils import data
+from models import custom_models
 
 
 class Taglet:
     """
     Taglet class
     """
-    def __init__(self):
-        raise NotImplementedError()
 
-    def execute(self, unlabeled_images, use_gpu=True):
+    def __init__(self, task):
+        """Initialize based on configuration dictionary"""
+
+        self.name = 'base'
+        self.task = task
+        self.lr = 0.0001
+        self.freezed_layer = 0
+        self.loss = torch.nn.CrossEntropyLoss()
+        self.seed = 0
+        self.num_epochs = 1
+        self.select_on_val = True # if true, save model on the best validation performance
+        self.batch_size = 32
+        self.pretrained = True #if true, we can load from pretrained model
+        self.num_workers = 2
+
+        if task.number_of_channels == 1:
+            self.model = custom_models.MnistResNet()
+            self.model.pretrained = self.pretrained
+        else:
+            self.model = models.resnet18(pretrained=self.pretrained)
+
+        self.model.fc = torch.nn.Linear(self.model.fc.in_features, len(self.task.classes))
+
+        ### parameters needed to be updated based on freezing layer
+        params_to_update = []
+        for name, param in self.model.named_parameters():
+            if param.requires_grad == True:
+                params_to_update.append(param)
+        self._params_to_update = params_to_update
+        self.optimizer = torch.optim.Adam(self._params_to_update, lr=self.lr, weight_decay=1e-4)
+        self.lr_scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=10, gamma=0.1)
+        self.save_dir = os.path.join('trained_models',str(task.task_id),self.name)
+        if not os.path.exists(self.save_dir):
+            os.makedirs(self.save_dir)
+
+        self.log = print
+        self._best_val_acc = 0.0
+        self.device = self._init_device()
+        self.model = self.model.to(self.device)
+        self.model.eval()
+
+        self.train_data_loader, self.val_data_loader, self.test_data_loader = self.load_data()
+
+    def _train_epoch(self):
         """
-        Top: I add use_gpu as another argument for this function.
-        Execute the taglet on a batch of images.
+        Training for an epoch
+        """
+        self.model.train()
+        running_loss = 0
+        running_acc = 0
+        for batch_idx, data in enumerate(self.train_data_loader):
+            inputs = (data[0]).to(self.device)
+            labels = (data[1]).to(self.device)
+
+            self.optimizer.zero_grad()
+            with torch.set_grad_enabled(True):
+                outputs = self.model(inputs)
+                loss = self.loss(outputs, labels)
+                _, preds = torch.max(outputs, 1)
+                loss.backward()
+                self.optimizer.step()
+
+            running_loss += loss.item()
+            running_acc += torch.sum(preds == labels.data)
+
+            if batch_idx >=1:
+                break
+
+        epoch_loss = running_loss / len(self.train_data_loader.dataset)
+        epoch_acc = running_acc.double() / len(self.train_data_loader.dataset)
+
+        return epoch_loss, epoch_acc
+
+    def _validate_epoch(self, data_loader):
+        """ validating/testing """
+
+        self.model.eval()
+        running_loss = 0
+        running_acc = 0
+        for batch_idx, data in enumerate(data_loader):
+            inputs = (data[0]).to(self.device)
+            labels = (data[1]).to(self.device)
+            with torch.set_grad_enabled(False):
+                outputs = self.model(inputs)
+                loss = self.loss(outputs, labels)
+                _, preds = torch.max(outputs, 1)
+
+            running_loss += loss.item()
+            running_acc += torch.sum(preds == labels.data)
+            if batch_idx >=2:
+                break
+
+        epoch_loss = running_loss / len(data_loader.dataset)
+        epoch_acc = running_acc.double() / len(data_loader.dataset)
+
+        return epoch_loss, epoch_acc
+
+    def train(self):
+        """
+        Training phase
+        """
+        best_model_to_save = None
+        for epoch in range(self.num_epochs):
+            self.log('epoch: {}'.format(epoch))
+            # train on training data
+            train_loss, train_acc = self._train_epoch()
+            self.log('train loss: {:.4f}'.format(train_loss))
+            self.log('train acc: {:.4f}%'.format(train_acc*100))
+
+
+            # evaluation on validation data
+            val_loss, val_acc = self._validate_epoch(self.val_data_loader)
+            self.log('validation loss: {:.4f}'.format(val_loss))
+            self.log('validation acc: {:.4f}%'.format(val_acc*100))
+
+            if self.lr_scheduler:
+                self.lr_scheduler.step()
+
+            if val_acc > self._best_val_acc:
+                self.log("Deep copying new best model. (validation of {:.4f}%, over {:.4f}%)".format(val_acc, self._best_val_acc))
+                self._best_val_acc = val_acc
+                best_model_to_save = copy.deepcopy(self.model.state_dict())
+                if self.select_on_val == True:
+                    best_model_wts = copy.deepcopy(self.model.state_dict())
+
+        if best_model_to_save != None:
+            torch.save(best_model_to_save, self.save_dir +  'model.pth.tar')
+
+        self.log("Epoch {} result: ".format(epoch + 1))
+        self.log("Average trainin loss: {:.4f}".format(train_loss))
+        self.log("Average training accuracy: {:.4f}%".format(train_acc * 100))
+        self.log("Average validation loss: {:.4f}".format(val_loss))
+        self.log("Average validation accuracy: {:.4f}%".format(val_acc * 100))
+
+        if self.select_on_val:
+            # Reloads best model weights
+            self.model.load_state_dict(best_model_wts)
+
+        test_loss, test_acc = self._validate_epoch(self.test_data_loader)
+        self.log('test loss: {:.4f}'.format(test_loss))
+        self.log('test acc: {:.4f}%'.format(test_acc * 100))
+
+    def _init_device(self):
+        """
+        setup GPU device if available
+        """
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        return device
+
+    def _init_random(seed):
+        torch.backends.cudnn.deterministic = True
+
+        random.seed(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+
+    def execute(self):
+        """
+        Execute the taglet on all unlabeled images.
         :return: A batch of labels
         """
-        raise NotImplementedError()
+        raise NotImplementedError
 
+    def load_data(self):
+
+        if self.task.number_of_channels == 3:
+            data_mean = [0.485, 0.456, 0.406]
+            data_std = [0.229, 0.224, 0.225]
+        elif self.task.number_of_channels == 1:
+            data_mean = [0.5]
+            data_std = [0.5]
+
+        transform = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=data_mean, std=data_std)
+        ])
+
+        image_names= [img_name for img_name,label in self.task.labeled_images]
+        image_labels = [label for img_name,label in self.task.labeled_images]
+        train_val_test_data = CustomDataSet(self.task.unlabeled_image_path, image_names, image_labels, transform,
+                                       self.task.number_of_channels)
+
+
+
+        # %70 for training, 15% for validation, and 15% for test
+        train_percent = 0.7
+        num_data = len(train_val_test_data)
+        indices = list(range(num_data))
+        train_split = int(np.floor(train_percent * num_data))
+        np.random.shuffle(indices)
+        train_idx = indices[:train_split]
+        val_test_data = indices[train_split:]
+        val_split = int(np.floor(len(val_test_data)/2))
+        valid_idx = val_test_data[:val_split]
+        test_idx = val_test_data[val_split:]
+
+        train_set = data.Subset(train_val_test_data, train_idx)
+        val_set = data.Subset(train_val_test_data, valid_idx)
+        test_set = data.Subset(train_val_test_data, test_idx)
+
+        # test_data = datasets.ImageFolder(self.task.test_image_path, transform= transform)
+
+        train_data_loader = torch.utils.data.DataLoader(train_set, batch_size=self.batch_size, shuffle=False,
+                                                            num_workers=self.num_workers)
+        val_data_loader = torch.utils.data.DataLoader(val_set, batch_size=self.batch_size, shuffle=False,
+                                                        num_workers=self.num_workers)
+        test_data_loader = torch.utils.data.DataLoader(test_set, batch_size=self.batch_size, shuffle=False,
+                                                            num_workers=self.num_workers)
+
+        self.log('number of training data: %d'%len(train_data_loader.dataset))
+        self.log('number of validation data: %d'%len(val_data_loader.dataset))
+        self.log('number of test data: %d'%len(test_data_loader.dataset))
+
+
+        return train_data_loader, val_data_loader, test_data_loader
+
+
+class FineTuenTaglet(Taglet):
+
+    def __init__(self, task):
+        super().__init__(task)
 
 
 class TransferTaglet(Taglet):
@@ -83,17 +309,6 @@ class TransferTaglet(Taglet):
             list_predicted_labels.append(predicted_labels.cpu().detach().numpy())
         all_predicted_labels = np.concatenate(list_predicted_labels)
         return all_predicted_labels
-
-
-class FineTuenTaglet(Taglet):
-    def __init__(self):
-        super().__init__()
-
-    def finetune(self):
-        raise NotImplementedError
-
-    def execute(self, unlabeled_images):
-        raise NotImplementedError
 
 
 class MTLTaglet(Taglet):
