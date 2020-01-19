eaa35f1ea5de0bcf017c6e6659ea11dc783796d4
elahehraisi <maman>
2020-01-18 21:05:52 -0500

    added a custom resnet module for MNIST, which has one channel

---
+++ models/custom_models.py
@@ -0,0 +1,10 @@
+from torchvision.models.resnet import ResNet, BasicBlock
+import torch
+
+class MnistResNet(ResNet):
+    def __init__(self):
+        super(MnistResNet, self).__init__(BasicBlock, [2, 2, 2, 2], num_classes=10)
+        self.conv1 = torch.nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+
+    def forward(self, x):
+        return torch.softmax(super(MnistResNet, self).forward(x), dim=-1)
