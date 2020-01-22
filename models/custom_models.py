48bd93d9aa144fa863c7e604de5698d9e3c82808
elahehraisi <maman>
2020-01-22 15:25:06 -0500

    added prototype modules

--- models/custom_models.py
+++ models/custom_models.py
@@ -1,5 +1,6 @@
 from torchvision.models.resnet import ResNet, BasicBlock
 import torch
+import torch.nn as nn
 
 
 class MnistResNet(ResNet):
@@ -20,3 +21,44 @@ class MnistResNet(ResNet):
         :return: The softmax output
         """
         return torch.softmax(super(MnistResNet, self).forward(x), dim=-1)
+
+
+class Linear(nn.Module):
+
+    def __init__(self, in_feature=64, out_feature=10):
+        super().__init__()
+        self.classifier = nn.Sequential(
+            nn.Linear(in_feature, out_feature)
+            )
+
+    def forward(self, x):
+        x = self.classifier(x)
+        return x
+        #return x.view(x.size(0), -1)
+
+
+class ConvEncoder(nn.Module):
+
+    def __init__(self, x_dim=1, hid_dim=64, z_dim=64):
+        super().__init__()
+        self.encoder = nn.Sequential(
+            self.conv_block(x_dim, hid_dim),
+            self.conv_block(hid_dim, hid_dim),
+            self.conv_block(hid_dim, hid_dim),
+            self.conv_block(hid_dim, z_dim),
+        )
+        self.out_channels = 1600
+
+    def forward(self, x):
+        x = self.encoder(x)
+        return x.view(x.size(0), -1)
+
+    def conv_block(self, in_channels, out_channels):
+        bn = nn.BatchNorm2d(out_channels)
+        nn.init.uniform_(bn.weight)  # for pytorch 1.2 or later
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, padding=1),
+            bn,
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
