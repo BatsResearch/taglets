312e74f8bb5d9cbff36a09c972a638845097fc1f
Jeffrey Zhu <jeffrey_zhu@brown.edu>
2020-03-06 17:09:25 -0500

    minor scads fixes

--- taglets/scads/create/create_scads.py
+++ taglets/scads/create/create_scads.py
@@ -2,6 +2,7 @@ import os
 import sqlalchemy as sa
 from sqlalchemy.orm import sessionmaker
 from .scads_classes import Node, Edge, Relation, Base
+from sqlalchemy import event
 
 
 def get_relations(directory):
@@ -30,8 +31,9 @@ def get_nodes(directory):
     all_nodes = []
     with open(os.path.join(cwd, directory, 'nodes.csv'), encoding="utf8") as node_file:
         for line in node_file:
-            if line.strip():
-                node_key, conceptnet_id = line.strip().split()
+            split = line.strip().split()
+            if split and len(split) == 2:
+                node_key, conceptnet_id = split
                 if conceptnet_id.startswith("/c/en/"):
                     node = Node(id=node_key, conceptnet_id=conceptnet_id)
                     all_nodes.append(node)
@@ -74,25 +76,26 @@ def add_conceptnet(path_to_database, directory):
     all_edges = get_edges(directory)
 
     # Generate database schema
-    Base.metadata.create_all(engine)
     session = Session()
+    session.execute('PRAGMA foreign_keys=ON')
+    Base.metadata.create_all(engine)
 
     session.add_all(all_relations)
     session.commit()
+    print("Added relations.")
     session.add_all(all_nodes)
     session.commit()
+    print("Added nodes.")
 
     included_edges = []
     for edge in all_edges:
-        relation = session.query(Relation).filter(Relation.id == edge.relation_type).first()
         start_node = session.query(Node).filter(Node.id == edge.start_node).first()
         end_node = session.query(Node).filter(Node.id == edge.end_node).first()
-        if relation and start_node and end_node:
-            edge.relation = relation
-            start_node.outgoing_edges.append(edge)
+        if start_node and end_node:
             included_edges.append(edge)
 
     session.add_all(included_edges)
-
     session.commit()
+    print("Added edges.")
+
     session.close()
