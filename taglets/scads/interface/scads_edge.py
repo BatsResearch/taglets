1e463c8713665b6e65f48e4546e8616cf6ac886a
Jeffrey Zhu <jeffrey_zhu@brown.edu>
2020-03-10 15:56:01 -0400

    minor bug fix in scads_edge

--- taglets/scads/interface/scads_edge.py
+++ taglets/scads/interface/scads_edge.py
@@ -2,11 +2,11 @@ class ScadsEdge:
     """
     A class representing a relationship between two nodes in the SCADS
     """
-    def __init__(self, start_node, end_node, relationship, is_directed):
+    def __init__(self, start_node, end_node, relationship, directed):
         self.start_node = start_node        # The starting ScadsNode in the edge
         self.end_node = end_node            # The ending ScadsNode in the edge
         self.relationship = relationship    # The type of relationship
-        self.is_directed = is_directed      # Whether or not the relationship is directed
+        self.directed = directed      # Whether or not the relationship is directed
 
     def get_end_node(self):
         return self.end_node
@@ -15,4 +15,4 @@ class ScadsEdge:
         return self.relationship
 
     def is_directed(self):
-        return self.is_directed
+        return self.directed
