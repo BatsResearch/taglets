a45e97698f56f0b13c25db210a380b6fbce2f581
topwasu <wasu.piri@gmail.com>
2020-07-06 15:11:22 -0400

    Pass in config to estimate_label_model

--- taglets/controller.py
+++ taglets/controller.py
@@ -186,9 +186,11 @@ class Controller:
 
     def _train_label_model(self, vote_matrix):
         log.info("Training label model")
+        config = labelmodels.LearningConfig()
+        config.epochs = 1
         labelmodel = labelmodels.NaiveBayes(
             num_classes=len(self.task.classes), num_lfs=vote_matrix.shape[1])
-        labelmodel.estimate_label_model(vote_matrix)
+        labelmodel.estimate_label_model(vote_matrix, config=config)
         log.info("Finished training label model")
         return labelmodel
 
