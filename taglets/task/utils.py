f0b830b7d453de03bbeb5a01353f81844592ba94
topwasu <wasu.piri@gmail.com>
2020-07-18 14:28:47 -0400

    Fix ScadsEmbedding path Fix tests to pass list of conceptnet ids to Task.classes

--- taglets/task/utils.py
+++ taglets/task/utils.py
@@ -1,5 +1,5 @@
 from ..scads import ScadsEmbedding
 
 def labels_to_concept_ids(labels):
-    ScadsEmbedding.load(r'C:\Users\wasup\Desktop\Classes\githubproject\taglets\taglets\sql_data\numberbatch-en-19.08.txt.gz')
+    ScadsEmbedding.load('/data/datasets/numberbatch-en-19.08.txt.gz')
     return [ScadsEmbedding.get_related_nodes('/c/en/' + label, limit=1, is_node=False)[0] for label in labels]
