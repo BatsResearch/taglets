f05a215238045e002eea331a54c875ef969525c7
Nihal Nayak <nihalnayak@gmail.com>
2020-07-29 09:53:29 -0400

    fix: testing

--- test/modules/test_zero_shot.py
+++ test/modules/test_zero_shot.py
@@ -8,7 +8,7 @@ from taglets.scads import Scads
 from taglets.scads.create.install import Installer, MnistInstallation
 from taglets.controller import Controller
 from taglets.task import Task
-from taglets.modules.zero_shot_kg import ZSLKGModule, ZSLKGTaglet
+from taglets.modules.zsl_kg_lite import ZSLKGModule, ZSLKGTaglet
 from taglets.modules.zsl_kg_lite.example_encoders.resnet import ResNet
 from taglets.data.custom_dataset import CustomDataset
 
@@ -30,7 +30,7 @@ class TestZeroShotModule(unittest.TestCase):
                     scads_path=None)
         cls.module = ZSLKGModule(task)
 
-        options = cls.module.taglets[0].zsl_kg.options
+        options = cls.module.taglets[0].options
 
 
         cls.adj_lists = {
@@ -43,8 +43,8 @@ class TestZeroShotModule(unittest.TestCase):
 
 
         # the graph neural network model
-        cls.taglet = cls.module.taglets[0].zsl_kg
-        cls.model = cls.module.taglets[0].zsl_kg._get_model(features, cls.adj_lists, device, options)
+        cls.taglet = cls.module.taglets[0]
+        cls.model = cls.module.taglets[0]._get_model(features, cls.adj_lists, device, options)
 
         cls.resnet = ResNet()
 
