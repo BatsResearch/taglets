f1b90ac52a70621be87ca0b09008a8b4c1844113
Stephen Bach <stephenhbach@gmail.com>
2020-08-01 10:07:39 -0400

    Reorganizing test data a bit.

--- test/modules/test_module.py
+++ test/modules/test_module.py
@@ -1,8 +1,33 @@
+import os
+import sqlalchemy as sa
+from sqlalchemy.orm import sessionmaker
+from taglets.scads.create.scads_classes import Node, Edge, Relation, Base
 import unittest
-import numpy as np
+
+TEST_DATA = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_data")
+DB_PATH = os.path.join(TEST_DATA, "test_scads.db")
 
 
 class TestModule(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        # Sets up test SCADS
+
+        # Generates database schema
+        engine = sa.create_engine('sqlite:///' + DB_PATH)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        session.execute('PRAGMA foreign_keys=ON')
+        Base.metadata.create_all(engine)
+
+        # Creates relations
+        rel1 = Relation(id=0, type="/r/IsA", is_directed=True)
+        rel2 = Relation(id=1, type="/r/RelatedTo", is_directed=False)
+        session.add(rel1)
+        session.add(rel2)
+
+        # Creates nodes
 
     def test_module(self):
 
--- test/test_controller.py
+++ test/test_controller.py
@@ -1,13 +1,20 @@
 from taglets.controller import Controller
+from taglets.scads import Scads
+from taglets.scads.create.install import Installer, MnistInstallation
 from taglets.task import Task
 
-import unittest
-
+import os
 import torch
-from torch.utils.data import DataLoader, Dataset, Subset
+from torch.utils.data import Dataset, Subset
 from torchvision import transforms
 from torchvision.datasets import MNIST
 from torchvision.models.resnet import ResNet, BasicBlock
+import unittest
+
+TEST_DATA = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_data/scads")
+DB_PATH = os.path.join(TEST_DATA, "test_scads.db")
+CONCEPTNET_PATH = os.path.join(TEST_DATA, "conceptnet")
+MNIST_PATH = "mnist"
 
 
 class HiddenLabelDataset(Dataset):
@@ -39,6 +46,13 @@ class MnistResNet(ResNet):
 
 
 class TestController(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # Set Up Scads
+        installer = Installer(DB_PATH)
+        installer.install_conceptnet(CONCEPTNET_PATH)
+        installer.install_dataset(TEST_DATA, MNIST_PATH, MnistInstallation())
+
     def test_mnist(self):
         # Creates task
         classes = ['/c/en/zero/n/wn/quantity',
@@ -72,6 +86,47 @@ class TestController(unittest.TestCase):
         mnist_test = MNIST('.', train=False, transform=preprocess, download=True)
         mnist_test = Subset(mnist_test, [i for i in range(1000)])
         self.assertGreater(end_model.evaluate(mnist_test, use_gpu=False), .9)
+
+    def test_mnist_with_scads(self):
+        # Creates task
+        classes = ['/c/en/zero/n/wn/quantity',
+                   '/c/en/one/n/wn/quantity',
+                   '/c/en/two/n/wn/quantity',
+                   '/c/en/three/n/wn/quantity',
+                   '/c/en/four/n/wn/quantity',
+                   '/c/en/five/n/wn/quantity',
+                   '/c/en/six/n/wn/quantity',
+                   '/c/en/seven/n/wn/quantity',
+                   '/c/en/eight/n/wn/quantity',
+                   '/c/en/nine/n/wn/quantity']
+
+        preprocess = transforms.Compose(
+            [transforms.Grayscale(num_output_channels=3),
+             transforms.ToTensor()])
+
+        mnist = MNIST('.', train=True, transform=preprocess, download=True)
+        size = int(len(mnist) / 50)
+        labeled = Subset(mnist, [i for i in range(size)])
+        unlabeled = HiddenLabelDataset(Subset(mnist, [i for i in range(size, 2 * size)]))
+        val = Subset(mnist, [i for i in range(2 * size, 3 * size)])
+        task = Task(
+            "mnist-test", classes, (28, 28), labeled, unlabeled, val, scads_path=DB_PATH
+        )
+        task.set_initial_model(MnistResNet())
+        Scads.set_root_path(TEST_DATA)
+
+        # Executes task
+        controller = Controller(task, use_gpu=False)
+        end_model = controller.train_end_model()
+
+        # Evaluates end model
+        mnist_test = MNIST('.', train=False, transform=preprocess, download=True)
+        mnist_test = Subset(mnist_test, [i for i in range(1000)])
+        self.assertGreater(end_model.evaluate(mnist_test, use_gpu=False), .9)
+
+    @classmethod
+    def tearDownClass(cls):
+        os.remove(DB_PATH)
 
 
 if __name__ == "__main__":
