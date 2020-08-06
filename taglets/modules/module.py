02acf88d771be7b1056e2df9513ddf20a220402b
Stephen Bach <stephenhbach@gmail.com>
2020-08-05 22:32:34 -0400

    First attempt at autoconfig.

--- taglets/modules/module.py
+++ taglets/modules/module.py
@@ -10,16 +10,15 @@ class Module:
         self.task = task
         self.taglets = []   # List of taglets must be defined in subclasses
 
-    def train_taglets(self, train_data, val_data, use_gpu):
+    def train_taglets(self, train_data, val_data):
         """
         Train the Module's taglets.
         :param train_data: A Torch Dataset of training data
         :param val_data: A Torch Dataset of validation data
-        :param use_gpu: Whether or not to use the GPU
         :return: None
         """
         for taglet in self.taglets:
-            taglet.train(train_data, val_data, use_gpu)
+            taglet.train(train_data, val_data)
 
     def get_taglets(self):
         """
