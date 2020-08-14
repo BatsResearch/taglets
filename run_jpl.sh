2cc92ff769a5dc783c8a0a4d554444568e05456c
topwasu <wasu.piri@gmail.com>
2020-08-14 16:30:05 -0400

    Change ulimit to 1,000,000 and edit README to add that /proc/sys/fs/file-max = 1,000,000 is a requirement (need sudo permission to set this)

--- run_jpl.sh
+++ run_jpl.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Increases open file limit for multiprocessing
-ulimit -n 65536
+ulimit -n 1000000
 export PYTHONPATH=".:$PYTHONPATH"
 python -m taglets.task.jpl --dataset_dir /lwll/development --scads_root_dir /lwll/external --task_ix 1
