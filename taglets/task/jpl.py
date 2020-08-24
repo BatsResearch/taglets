81247f1e3ae3820b21bef4a71fd128591a9626fc
topwasu <wasu.piri@gmail.com>
2020-08-23 20:35:04 -0400

    Fix run_random_checkpoint #5

--- taglets/task/jpl.py
+++ taglets/task/jpl.py
@@ -420,14 +420,13 @@ class JPLRunner:
         elif checkpoint_num > 2:
             candidates = self.confidence_active_learning.find_candidates(available_budget, unlabeled_image_names)
             self.request_labels(candidates)
-            new_candidates = list(range(len(self.jpl_storage.get_unlabeled_image_names())))
-            import random
-            random.shuffle(new_candidates)
-            self.confidence_active_learning.set_candidates(new_candidates)
-        
         predictions_dict = {'id': self.jpl_storage.get_evaluation_image_names(),
                             'class': [self.jpl_storage.classes[0]] * len(self.jpl_storage.get_evaluation_image_names())}
         self.submit_predictions(predictions_dict)
+        new_candidates = list(range(len(self.jpl_storage.get_unlabeled_image_names())))
+        import random
+        random.shuffle(new_candidates)
+        self.confidence_active_learning.set_candidates(new_candidates)
 
     def run_one_checkpoint(self, phase, checkpoint_num):
         log.info('------------------------------------------------------------')
