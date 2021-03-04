0cce7088e6bd17bec8a77f22892e3718c240ff0e
Cristina <cricri.menghini@gmail.com>
2021-03-04 23:32:12 +0000

    [upd] dev and prod lauching modes

---
+++ dev_config.py
@@ -0,0 +1,9 @@
+dataset_type ='sample'
+problem_type = 'image_classification'
+dataset_dir = '/lwll/development'
+api_url = 'https://api-dev.lollllz.com/'
+problem_task = '6d5e1f85-5d8f-4cc9-8184-299db03713f4'
+team_secret = 'a5aed2a8-db80-4b22-bf72-11f2d0765572'
+gov_team_secret = 'mock-secret'
+data_paths = ('predefined/scads.fall2020.sqlite3',
+              'predefined/embeddings/numberbatch-en19.08.txt.gz')
