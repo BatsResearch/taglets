ef41b3426bab3abedc96637d31900a01972ea6b4
topwasu <wasu.piri@gmail.com>
2021-03-16 03:32:10 -0400

    Make NaiveVideoTaglet override _validate_epoch

--- taglets/modules/naive_video.py
+++ taglets/modules/naive_video.py
@@ -75,4 +75,38 @@ class NaiveVideoTaglet(Taglet):
         epoch_acc = running_acc.item() / len(train_data_loader.dataset)
 
         return epoch_loss, epoch_acc
+    
+    def _validate_epoch(self, rank, val_data_loader,):
+        """
+        Train for one epoch.
+        :param train_data_loader: A dataloader containing training videos
+        :param use_gpu: Whether or not to use the GPU
+        :return: None
+        """
+        self.model.eval()
+        running_loss = 0
+        running_acc = 0
+        for batch in val_data_loader:
+            inputs = batch[0]
+            labels = batch[1]
+            if self.use_gpu:
+                inputs = inputs.cuda(rank)
+                labels = labels.cuda(rank)
+            num_videos = inputs.size(0)
+            num_frames = inputs.size(1)
+            inputs = inputs.flatten(start_dim=0, end_dim=1)
+
+            with torch.set_grad_enabled(False):
+                outputs = self.model(inputs)
+                aggregated_outputs = torch.mean(outputs.view(num_videos, num_frames, -1), dim=1)
+                loss = self.criterion(aggregated_outputs, labels)
+                _, preds = torch.max(aggregated_outputs, 1)
+
+            running_loss += loss.item()
+            running_acc += self._get_train_acc(aggregated_outputs, labels)
+
+        epoch_loss = running_loss / len(val_data_loader.dataset)
+        epoch_acc = running_acc.item() / len(val_data_loader.dataset)
+
+        return epoch_loss, epoch_acc
 
