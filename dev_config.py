1fb99391ce6215f056e109fda7990c53be4586bd
Cristina <cricri.menghini@gmail.com>
2021-03-25 15:52:29 +0000

    [fix] add arg to metadocker cong

--- dev_config.py
+++ dev_config.py
@@ -1,8 +1,8 @@
-dataset_type ='sample'
-problem_type = 'video_classification'
+dataset_type ='full'
+problem_type = 'image_classification'
 dataset_dir = '/lwll/development'
 api_url = 'https://api-dev.lollllz.com/'
-problem_task = 'problem_test_video_classification'
+problem_task = 'problem_test_image_classification'
 team_secret = 'a5aed2a8-db80-4b22-bf72-11f2d0765572'
 gov_team_secret = 'mock-secret'
 data_paths = ('predefined/scads.fall2020.sqlite3',
