6e8e560de6cd6e6f26836a77f8bcdb9f303074a7
topwasu <wasu.piri@gmail.com>
2021-04-21 11:28:03 -0400

    Rename TrainableMixin to TagletMixin

--- taglets/pipeline/taglet.py
+++ taglets/pipeline/taglet.py
@@ -4,7 +4,7 @@ import numpy as np
 
 log = logging.getLogger(__name__)
 
-class TrainableMixin:
+class TagletMixin:
     def execute(self, unlabeled_data):
         """
         Execute the Taglet on unlabeled images.
@@ -16,13 +16,13 @@ class TrainableMixin:
         return np.argmax(outputs, 1)
 
 
-class ImageTaglet(ImageTrainable, TrainableMixin):
+class ImageTaglet(ImageTrainable, TagletMixin):
     """
     A trainable model that produces votes for unlabeled images
     """
 
 
-class VideoTaglet(VideoTrainable, TrainableMixin):
+class VideoTaglet(VideoTrainable, TagletMixin):
     """
     A trainable model that produces votes for unlabeled videos
     """
