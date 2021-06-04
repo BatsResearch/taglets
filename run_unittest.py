50f0744667aef969113ef4b42fae1db8551ab926
topwasu <wasu.piri@gmail.com>
2021-06-04 13:31:40 -0400

    Revert "Set up multi-process unittest"
    This reverts commit 2eb2a9d2515507da112375395a39ebb2ad178b14.

--- run_unittest.py
+++
@@ -1,7 +0,0 @@
-import unittest
-
-if __name__ == '__main__':
-    loader = unittest.TestLoader()
-    tests = loader.discover('./test')
-    testRunner = unittest.runner.TextTestRunner()
-    testRunner.run(tests)
