7f297af44881853781203c440932ad05b6513425
topwasu <wasu.piri@gmail.com>
2021-06-29 22:31:33 -0400

    fix bug (nonzero returns a tuple)

--- taglets/modules/dann.py
+++ taglets/modules/dann.py
@@ -191,7 +191,7 @@ class DannTaglet(ImageTaglet):
             image_paths = []
             image_labels = []
             for i in range(all_related_class):
-                indices = np.nonzero(old_image_labels == i)
+                indices = np.nonzero(old_image_labels == i)[0]
                 new_indices = np.random.choice(indices, self.img_per_related_class, replace=False)
                 image_paths.extend(list(old_image_paths[new_indices]))
                 image_labels.extend([i] * self.img_per_related_class)
