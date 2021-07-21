01284e6d372ecba607443c2379fa377c173e5327
topwasu <wasu.piri@gmail.com>
2021-07-20 22:39:45 -0400

    Automatically set num_processes equal to the number of gpu available (LWLL_TA1_GPUS)

--- run_jpl.sh
+++ run_jpl.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+gpu_array=($LWLL_TA1_GPUS)
+sed -i "/num_processes:/c\num_processes: ${#gpu_array[@]}" accelerate_config.yml
+
 accelerate launch --config_file ./accelerate_config.yml ./run_jpl.py --mode prod --simple_version false --folder evaluation
