05d36bc23ad075a99a144ac14b7c145e8cbb4147
topwasu <wasu.piri@gmail.com>
2021-07-21 17:50:08 -0400

    Handle the case where LWLL_TA1_GPU="all"

--- run_jpl.sh
+++ run_jpl.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 gpu_array=($LWLL_TA1_GPUS)
-sed -i "/num_processes:/c\num_processes: ${#gpu_array[@]}" accelerate_config.yml
+if [[ $LWLL_TA1_GPU == "all" ]]; then
+    sed -i "/num_processes:/c\num_processes: $(nvidia-smi --list-gpus | wc -l)" accelerate_config.yml
+else
+    sed -i "/num_processes:/c\num_processes: ${#gpu_array[@]}" accelerate_config.yml
+fi
 
 accelerate launch --config_file ./accelerate_config.yml ./run_jpl.py --mode prod --simple_version false --folder evaluation
