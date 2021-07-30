a5311580afcbdd64fbc23593399563f2ed18db31
Cristina Menghini <cricri.menghini@gmail.com>
2021-07-30 19:35:54 +0200

    retrieve labels prev checkpoints

--- taglets/task/jpl.py
+++ taglets/task/jpl.py
@@ -134,6 +134,7 @@ class JPL:
         log.debug(f"HEADERS: {headers}")
         response = self.get_only_once("seed_labels", headers)
         labels = response['Labels']
+        #log.info(f"NUMBER OF LABELED DATA: {len(labels)}")
 
         if video:
             seed_labels = []
@@ -644,14 +645,9 @@ class JPLRunner:
 
         start_time = time.time()
 
-
+        #log.info(f"session STATUS {self.api.get_session_status()}")
         # Skip checkpoint before getting available budget
-        if checkpoint_num == 6:
-            log.info('{} Skip Checkpoint: {} Elapsed Time =  {}'.format(phase,
-                                                               checkpoint_num,
-                                                               time.strftime("%H:%M:%S",
-                                                                             time.gmtime(time.time()-start_time))))
-            return self.api.skip_checkpoint()
+        
 
         available_budget = self.get_available_budget()
         if checkpoint_num == 0:
@@ -669,6 +665,8 @@ class JPLRunner:
         unlabeled_image_names = self.jpl_storage.get_unlabeled_image_names(self.jpl_storage.dictionary_clips,
                                                                            self.video)
         log.debug('Number of unlabeled data: {}'.format(len(unlabeled_image_names)))
+
+        
         
         if checkpoint_num >= 4:
             """ For the last evaluation we used to start asking for custom labels after the first 2 checkpoints.
@@ -688,7 +686,18 @@ class JPLRunner:
             """
             candidates = self.random_active_learning.find_candidates(available_budget, unlabeled_image_names)
             log.debug(f"Candidates to query[0]: {candidates[0]}")
+            #log.info(f"NUMBER OF LABELED DATA: {len(labels)}")
             self.request_labels(candidates, self.video)
+
+            if checkpoint_num == 6:
+                candidates = self.random_active_learning.find_candidates(available_budget, unlabeled_image_names)
+                self.request_labels(candidates, self.video)
+                
+                log.info('{} Skip Checkpoint: {} Elapsed Time =  {}'.format(phase,
+                                                                checkpoint_num,
+                                                                time.strftime("%H:%M:%S",
+                                                                                time.gmtime(time.time()-start_time))))
+                return self.api.skip_checkpoint()
 
         labeled_dataset, val_dataset = self.jpl_storage.get_labeled_dataset(checkpoint_num, self.jpl_storage.dictionary_clips, self.video)
         unlabeled_train_dataset = self.jpl_storage.get_unlabeled_dataset(True, self.video)
