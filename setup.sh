9f477f9153505ed21a247dd9009e4d583c6089da
topwasu <wasu.piri@gmail.com>
2021-08-23 19:56:43 -0400

    Suppress wget's outputs

--- setup.sh
+++ setup.sh
@@ -8,19 +8,19 @@ mkdir -p predefined/embeddings
 mkdir -p predefined/zsl_kg_lite
 
 cd predefined
-wget -nc https://storage.googleapis.com/taglets-public/scads.spring2021.sqlite3
+wget -nc -q https://storage.googleapis.com/taglets-public/scads.spring2021.sqlite3
 
 cd embeddings
-wget -nc https://storage.googleapis.com/taglets-public/embeddings/glove.840B.300d.txt.gz
+wget -nc -q https://storage.googleapis.com/taglets-public/embeddings/glove.840B.300d.txt.gz
 gunzip --force < glove.840B.300d.txt.gz > glove.840B.300d.txt
 
-wget -nc https://storage.googleapis.com/taglets-public/embeddings/numberbatch-en19.08.txt.gz
+wget -nc -q https://storage.googleapis.com/taglets-public/embeddings/numberbatch-en19.08.txt.gz
 
-wget -nc https://storage.googleapis.com/taglets-public/embeddings/spring2021_processed_numberbatch.h5
+wget -nc -q https://storage.googleapis.com/taglets-public/embeddings/spring2021_processed_numberbatch.h5
 
 cd ../zsl_kg_lite
-wget -nc https://storage.googleapis.com/taglets-public/zsl_kg_lite/transformer.pt
+wget -nc -q https://storage.googleapis.com/taglets-public/zsl_kg_lite/transformer.pt
 
 cd ../
-wget -nc https://storage.googleapis.com/taglets-public/protonet-trained.pth
+wget -nc -q https://storage.googleapis.com/taglets-public/protonet-trained.pth
 
