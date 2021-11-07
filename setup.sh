fe516974982cd53a373f30741e08dd316dea72a9
topwasu <wasu.piri@gmail.com>
2021-11-07 17:32:36 -0500

    Download ImageNet21k SCADS files

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ mkdir -p predefined/zsl_kg_lite
 
 cd predefined
 wget -nc https://storage.googleapis.com/taglets-public/scads.spring2021.sqlite3
+wget -nc https://storage.googleapis.com/taglets-public/scads.imagenet22k.sqlite3
 wget -nc https://storage.googleapis.com/bit_models/BiT-M-R50x1.npz
 
 cd embeddings
@@ -16,8 +17,8 @@ wget -nc https://storage.googleapis.com/taglets-public/embeddings/glove.840B.300
 gunzip --force < glove.840B.300d.txt.gz > glove.840B.300d.txt
 
 wget -nc https://storage.googleapis.com/taglets-public/embeddings/numberbatch-en19.08.txt.gz
-
 wget -nc https://storage.googleapis.com/taglets-public/embeddings/spring2021_processed_numberbatch.h5
+wget -nc https://storage.googleapis.com/taglets-public/embeddings/imagenet22k_processed_numberbatch.h5
 
 cd ../zsl_kg_lite
 wget -nc https://storage.googleapis.com/taglets-public/zsl_kg_lite/transformer.pt
