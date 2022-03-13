31074940da82d2b503809d6fd0eb8d436d20e9fc
topwasu <wasu.piri@gmail.com>
2022-03-13 19:06:16 -0400

    Clean setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ mkdir -p predefined/embeddings
 mkdir -p predefined/zsl_kg_lite
 
 cd predefined
-wget -nc https://storage.googleapis.com/taglets-public/scads.spring2021.sqlite3
+wget -nc https://storage.googleapis.com/taglets-public/scads.imagenet22k-mod.sqlite3
 wget -nc https://storage.googleapis.com/bit_models/BiT-M-R50x1.npz
 
 cd embeddings
@@ -17,20 +17,7 @@ gunzip --force < glove.840B.300d.txt.gz > glove.840B.300d.txt
 
 wget -nc https://storage.googleapis.com/taglets-public/embeddings/numberbatch-en19.08.txt.gz
 
-wget -nc https://storage.googleapis.com/taglets-public/embeddings/spring2021_processed_numberbatch.h5
+wget -nc https://storage.googleapis.com/taglets-public/embeddings/imagenet22k-mod_processed_numberbatch.h5
 
 cd ../zsl_kg_lite
 wget -nc https://storage.googleapis.com/taglets-public/zsl_kg_lite/transformer.pt
-
-cd ../
-wget -nc https://storage.googleapis.com/taglets-public/protonet-trained.pth
-
-
-cd ../pseudoshots
-wget -nc https://storage.googleapis.com/taglets-public/pseudoshots/resnet50_train_val_test_224_1shot.pth
-wget -nc https://storage.googleapis.com/taglets-public/pseudoshots/simclr50.pth
-
-wget -nc https://storage.googleapis.com/taglets-public/pseudoshots/resnet12.pth
-wget -nc https://storage.googleapis.com/taglets-public/pseudoshots/resnet12_mask.pth
-
-wget -nc https://storage.googleapis.com/taglets-public/pseudoshots/resnet50-224-mini.pth
