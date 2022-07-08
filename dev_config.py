ec3563d814c24250fcc0faff29e86d6386302352
Cristina Menghini <cricri.menghini@gmail.com>
2022-07-08 13:15:24 +0200

    change labelmodel

--- dev_config.py
+++ dev_config.py
@@ -1,7 +1,7 @@
 dataset_type ='full'
 problem_type = 'image_classification'
 dataset_dir = '/users/cmenghin/data/bats/datasets/lwll'
-api_url = 'https://api-staging.lollllz.com/'
+api_url = 'https://api-staging.lollllz.com'
 problem_task = 'bbfadb2c-c7c3-4596-b548-3dd01a6d1d2c'
 team_secret = 'a5aed2a8-db80-4b22-bf72-11f2d0765572'
 gov_team_secret = 'mock-secret'
