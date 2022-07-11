ba857cedbe28cdc53ac787ae6efc93a24b2f9b11
Cristina Menghini <cricri.menghini@gmail.com>
2022-07-11 12:35:15 +0200

    check

--- taglets/modules/module.py
+++ taglets/modules/module.py
@@ -10,7 +10,7 @@ class Module:
         self.task = task
         self.taglets = []   # List of taglets must be defined in subclasses
 
-    def train_taglets(self, train_data, val_data, unlabeled_data=None, num_checkpoint=None):
+    def train_taglets(self, train_data, val_data, unlabeled_data=None):
         """
         Train the Module's taglets.
         :param train_data: A Torch Dataset of training data
@@ -19,7 +19,7 @@ class Module:
         :return: None
         """
         for taglet in self.taglets:
-            taglet.train(train_data, val_data, unlabeled_data, num_checkpoint=num_checkpoint)
+            taglet.train(train_data, val_data, unlabeled_data)
             
     def get_valid_taglets(self):
         """
