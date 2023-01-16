8320ea278f5ebfbea741d1f28655d7a150007c04
Cristina Menghini <cricri.menghini@gmail.com>
2023-01-16 16:48:00 -0500

    url api

--- dev_config.py
+++ dev_config.py
@@ -1,7 +1,7 @@
 dataset_type ='full'
 problem_type = 'image_classification'
 dataset_dir = '/users/cmenghin/data/bats/datasets/lwll'#'/lwll'
-api_url = 'https://api-dev.lollllz.com/'
+api_url = 'https://api-dev.lollllz.com'
 problem_task = '6d5e1f85-5d8f-4cc9-8184-299db03713f4'#'problem_test_image_classification'
 team_secret = 'a5aed2a8-db80-4b22-bf72-11f2d0765572'
 gov_team_secret = 'mock-secret'
