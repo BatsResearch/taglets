215fcb9ee1dfd81334b291428db669dc88aee5f1
Cristina Menghini <cricri.menghini@gmail.com>
2023-02-13 12:55:51 -0500

    fix indentation

--- system/zsl_jpl.py
+++ system/zsl_jpl.py
@@ -453,10 +453,10 @@ def workflow(dataset_type, dataset_dir, api_url,
     # Submit predictions (standard)
     if accelerator.is_local_main_process:
         std_response = api.submit_standard_zsl(std_predictions.to_dict())
-    log.info(f'Standard ZSL results')
-    log.info(f"[STD] Unseen accuracy: {std_response['Session_Status']['standard_zsl_scores']['accuracy_unseen_std']}")
-    log.info(f"[STD] Unseen average recall per class: {std_response['Session_Status']['standard_zsl_scores']['average_per_class_recall_unseen_std']}")
-    
+        log.info(f'Standard ZSL results')
+        log.info(f"[STD] Unseen accuracy: {std_response['Session_Status']['standard_zsl_scores']['accuracy_unseen_std']}")
+        log.info(f"[STD] Unseen average recall per class: {std_response['Session_Status']['standard_zsl_scores']['average_per_class_recall_unseen_std']}")
+        
     # Validate on test set (general)
     gen_predictions = model.test_predictions(test_dataset,
                                              standard_zsl=False)
@@ -464,11 +464,11 @@ def workflow(dataset_type, dataset_dir, api_url,
     # Submit predictions (general)
     if accelerator.is_local_main_process:
         gen_response = api.submit_generalized_zsl(gen_predictions.to_dict())
-    log.info(f'Generalized ZSL results')
-    log.info(f"[GEN] Accuracy all classes: {gen_response['Session_Status']['checkpoint_scores'][0]['accuracy_all_classes']}")
-    log.info(f"[GEN] Accuracy seen classes: {gen_response['Session_Status']['checkpoint_scores'][0]['accuracy_seen']}")
-    log.info(f"[GEN] Accuracy unseen classes: {gen_response['Session_Status']['checkpoint_scores'][0]['accuracy_unseen']}")
-    
+        log.info(f'Generalized ZSL results')
+        log.info(f"[GEN] Accuracy all classes: {gen_response['Session_Status']['checkpoint_scores'][0]['accuracy_all_classes']}")
+        log.info(f"[GEN] Accuracy seen classes: {gen_response['Session_Status']['checkpoint_scores'][0]['accuracy_seen']}")
+        log.info(f"[GEN] Accuracy unseen classes: {gen_response['Session_Status']['checkpoint_scores'][0]['accuracy_unseen']}")
+        
 
 
 def main():
