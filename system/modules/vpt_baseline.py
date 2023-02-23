9565c8ef568e403d85aee8b396fc246a5d4f8b55
Cristina Menghini <cricri.menghini@gmail.com>
2023-02-23 08:54:52 -0500

    get tensor item

--- system/modules/vpt_baseline.py
+++ system/modules/vpt_baseline.py
@@ -318,7 +318,7 @@ class VPTBaseline(object):
         unseen_labs = labels_outputs[unseen_true]
         unseen_accuracy = torch.sum(unseen_preds == unseen_labs)/len(unseen_true)
         
-        accuracy = st.hmean([unseen_accuracy, seen_accuracy])
+        accuracy = st.hmean([unseen_accuracy.item(), seen_accuracy.item()])
 
         #accuracy = torch.sum(predictions_outputs == labels_outputs)/len(predictions_outputs)
         log.info(F"Training SEEN accuracy after Epoch {epoch}: {seen_accuracy}")
