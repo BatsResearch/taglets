9331f25683ceb1bc95cefb72306cb90f1785e632
Cristina Menghini <cricri.menghini@gmail.com>
2023-02-28 11:50:11 -0500

    remove modules from init

--- system/modules/__init__.py
+++ system/modules/__init__.py
@@ -1,11 +1,1 @@
-from .clip_baseline import ClipBaseline
-from .coop_baseline import CoopBaseline, TextPrefixModel
-from .tpt_baseline import TptBaseline
-from .vpt_baseline import VPTBaseline
-from .adjust_and_adapt import AdjustAndAdapt
-from .vpt_pseudo_baseline import VPTPseudoBaseline
-from .coop_pseudo_baseline import CoopPseudoBaseline
 from .teacher_student import TeacherStudent
-from .disambiguate_teacher_student import DisambiguateTeacherStudent
-from .vpt_pseudo_disambiguate import VPTPseudoDisambiguate
-from .two_stage_classifier import TwoStageClassifier
