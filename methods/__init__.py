2e0ce61f1f9ea7d5a040bc6f916bb5a00a3919fb
Cristina Menghini <cricri.menghini@gmail.com>
2023-03-05 21:40:07 -0500

    change order imports

--- methods/__init__.py
+++ methods/__init__.py
@@ -1,6 +1,6 @@
 from .clip_baseline import ClipBaseline
 from .coop_baseline import CoopBaseline
 from .coop_pseudo_baseline import CoopPseudoBaseline
-from .teacher_student import TeacherStudent
 from .vpt_baseline import VPTBaseline
 from .vpt_pseudo_baseline import VPTPseudoBaseline
+from .teacher_student import TeacherStudent
