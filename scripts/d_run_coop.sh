ab5abf0e79e83271a0b75825d5c9297e748d2b5c
Cristina Menghini <cricri.menghini@gmail.com>
2023-03-06 12:00:54 -0500

    complete splits + set jobs for coop

--- scripts/d_run_coop.sh
+++ scripts/d_run_coop.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=E-clip
-#SBATCH --output=logs/all_clip.out
+#SBATCH --job-name=D-coop
+#SBATCH --output=logs/dtd_coop.out
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
-#SBATCH -t 12:00:00
+#SBATCH -t 24:00:00
 #SBATCH -p gpu --gres=gpu:4
 ##SBATCH --exclude=gpu[717-718,1201-1204,1209,1403]
 
@@ -17,9 +17,9 @@ sleep $[ ( $RANDOM % 30 )  + 1 ]s
 
 sed -i 's/^\(\s*main_process_port\s*:\s*\).*/\12072/'  accelerate_config.yml
 for vis_encoder in 'RN50' 'RN101' 'ViT-B/32' 'ViT-L/14'; do 
+for dataset_name in DTD; do
 for split_seed in 500 0 200; do
-for optim_seed in 10; do
-for dataset_name in EuroSAT DTD RESICS45 FGVCAircraft MNIST Flowers102; do
+for optim_seed in 10 100 50 400 250; do
 
     export OPTIM_SEED="$optim_seed"
     export VIS_ENCODER="$vis_encoder"
@@ -27,7 +27,7 @@ for dataset_name in EuroSAT DTD RESICS45 FGVCAircraft MNIST Flowers102; do
     export SPLIT_SEED="$split_seed"
     
     accelerate launch --config_file ./accelerate_config.yml ./run_main.py \
-                    --model_config clip_baseline_config.yml
+                    --model_config coop_baseline_config.yml
 done
 done
 done
