0c7f86e246dd6744524fd657483f9325e2680b58
Cristina Menghini <cricri.menghini@gmail.com>
2023-03-08 17:02:35 -0500

    shape image pref

--- models/clip_encoders.py
+++ models/clip_encoders.py
@@ -149,10 +149,11 @@ class CustomVisionTransformer(nn.Module):
 
         log.info(f"SHAPE flatten patches: {x.shape}")
         log.info(f"SHAPE prefix to concat: {image_prefix.size()}")
-
+        log.info(f"SHAPE prefix to concat: {image_prefix.unsqueeze(0)}")
+        
         # Here we concat the prefix to the flattened patches
         x = torch.cat([
-            image_prefix.unsqueeze(0), 
+            image_prefix, 
             x,
         ],
         dim=1,)
