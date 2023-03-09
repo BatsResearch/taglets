8217fa409e03883ebbb8a875b28f5f4fac9eb248
Cristina Menghini <cricri.menghini@gmail.com>
2023-03-08 21:13:13 -0500

    vit-b

--- scripts/r_run_base_methods.sh
+++ scripts/r_run_base_methods.sh
@@ -16,7 +16,7 @@ source ../../zsl/bin/activate
 sleep $[ ( $RANDOM % 30 )  + 1 ]s
 
 
-for vis_encoder in 'ViT-L/14'; do #  'ViT-B/32' 'RN50' 'ViT-L/14' 'RN101'
+for vis_encoder in 'ViT-B/32'; do # 'ViT-L/14'  'RN50' 'ViT-L/14' 'RN101'
 for split_seed in 500; do #  0 200
 for dataset_name in RESICS45; do
 for model in vpt_baseline; do 
