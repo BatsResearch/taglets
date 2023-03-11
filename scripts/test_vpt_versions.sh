9163d21d034357d07101202daa5c6fb4a1b39ce0
Cristina Menghini <cricri.menghini@gmail.com>
2023-03-11 12:25:31 -0500

    pass var

--- scripts/test_vpt_versions.sh
+++ scripts/test_vpt_versions.sh
@@ -2,8 +2,8 @@
 
 port=1270
 
-for type in after_norm_prefix; do # before_norm_cls before_norm_prefix; do # after_norm_cls
-for prefix_size in 16; do # 32; do
+for type in after_norm_prefix before_norm_cls before_norm_prefix; do # after_norm_cls
+for prefix_size in 16 32; do
 
     sbatch --export=TYPE=$type,PREFIX_SIZE=$prefix_size,PORT=$port --output=logs/test_vpt_${type}_${prefix_size}.out scripts/test_vpt_run.sh 
     port=port+1
