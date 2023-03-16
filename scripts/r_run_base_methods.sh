482886d435f096d6e88ff8994ab9654848dc708a
Cristina Menghini <cricri.menghini@gmail.com>
2023-03-16 09:56:28 -0400

    new runs

--- scripts/r_run_base_methods.sh
+++ scripts/r_run_base_methods.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=R-base_meth
-#SBATCH --output=logs/recsis45_vpt_base_methods_split_0.out
+#SBATCH --output=logs/recsis45_base_methods_split_200.out
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
-#SBATCH -t 48:00:00
+#SBATCH -t 72:00:00
 #SBATCH -p gpu --gres=gpu:4
 #SBATCH --exclude=gpu[717-718,1201-1204,1209,1403]
 
@@ -17,9 +17,9 @@ sleep $[ ( $RANDOM % 30 )  + 1 ]s
 
 
 for vis_encoder in 'ViT-B/32' 'ViT-L/14'; do # 'ViT-B/32'  'RN50' 'ViT-L/14' 'RN101'
-for split_seed in 0; do #  0 200
+for split_seed in 200; do #  0 200
 for dataset_name in RESICS45; do
-for model in vpt_baseline; do # coop_baseline
+for model in coop_baseline vpt_baseline; do # 
 for optim_seed in 1 2 3 4 5; do #10 100 50 400 250; do
 
     export OPTIM_SEED="$optim_seed"
@@ -28,7 +28,7 @@ for optim_seed in 1 2 3 4 5; do #10 100 50 400 250; do
     export SPLIT_SEED="$split_seed"
     export MODEL="$model"
     
-    sed -i 's/^\(\s*main_process_port\s*:\s*\).*/\12072/'  accelerate_config.yml
+    sed -i 's/^\(\s*main_process_port\s*:\s*\).*/\12076/'  accelerate_config.yml
     accelerate launch --config_file ./accelerate_config.yml ./run_main.py \
                     --model_config ${model}_config.yml
 done
