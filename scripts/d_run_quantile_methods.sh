a7eea23d6d69d4eca7ddc941e0b9a34f0f28bc79
Cristina Menghini <cricri.menghini@gmail.com>
2023-03-22 16:42:59 -0400

    f and d

---
+++ scripts/d_run_quantile_methods.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --job-name=D-quantile_meth
+#SBATCH --output=logs/dtd_quantile.out
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64G
+#SBATCH -t 96:00:00
+#SBATCH -p gpu --gres=gpu:4
+#SBATCH --exclude=gpu[717-718,1201-1204,1209,1403]
+
+module load cuda/11.1.1
+
+pwd
+source ../../zsl/bin/activate
+
+sleep $[ ( $RANDOM % 30 )  + 1 ]s
+
+for vis_encoder in 'ViT-B/32'; do # 'ViT-B/32' 'RN50' 'ViT-L/14' 'RN101'
+for split_seed in 500; do #  0 200
+for dataset_name in DTD; do
+for model in quantile_coop_pseudo_baseline quantile_vpt_pseudo_baseline; do # coop_pseudo_baseline
+for optim_seed in 1 2 3 4 5; do
+
+    export OPTIM_SEED="$optim_seed"
+    export VIS_ENCODER="$vis_encoder"
+    export DATASET_NAME="$dataset_name"
+    export SPLIT_SEED="$split_seed"
+    export MODEL="$model"
+    
+    sed -i 's/^\(\s*main_process_port\s*:\s*\).*/\12075/'  accelerate_config.yml
+    accelerate launch --config_file ./accelerate_config.yml ./run_main.py \
+                    --model_config ${model}_config.yml
+done
+done
+done
+done
+done
