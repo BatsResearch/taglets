510b62302a3335b4a5982a470c2ad26b5d44753e
Cristina Menghini <cricri.menghini@gmail.com>
2023-03-22 17:39:44 -0400

    all data

--- scripts/d_run_all_vpt_methods.sh
+++ scripts/d_run_all_vpt_methods.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=R-all_pseudo_meth
-#SBATCH --output=logs/resiscs45_all_pseudo_methods_split_500.out
+#SBATCH --job-name=D-all_pseudo_meth
+#SBATCH --output=logs/dtd_all_pseudo_methods.out
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
@@ -15,12 +15,11 @@ source ../../zsl/bin/activate
 
 sleep $[ ( $RANDOM % 30 )  + 1 ]s
 
-
 for vis_encoder in 'ViT-B/32'; do # 'ViT-B/32' 'RN50' 'ViT-L/14' 'RN101'
 for split_seed in 500; do #  0 200
-for dataset_name in RESICS45; do
-for model in all_vpt_pseudo_baseline; do #coop_pseudo_baseline vpt_pseudo_baseline; do #  all_vpt_pseudo_baseline; do 
-for optim_seed in 1 2 3 4 5; do # 10 100 50 400 250; do
+for dataset_name in DTD; do
+for model in all_vpt_pseudo_baseline; do # coop_pseudo_baseline
+for optim_seed in 1 2 3 4 5; do
 
     export OPTIM_SEED="$optim_seed"
     export VIS_ENCODER="$vis_encoder"
@@ -28,7 +27,7 @@ for optim_seed in 1 2 3 4 5; do # 10 100 50 400 250; do
     export SPLIT_SEED="$split_seed"
     export MODEL="$model"
     
-    sed -i 's/^\(\s*main_process_port\s*:\s*\).*/\12075/'  accelerate_config.yml
+    sed -i 's/^\(\s*main_process_port\s*:\s*\).*/\12076/'  accelerate_config.yml
     accelerate launch --config_file ./accelerate_config.yml ./run_main.py \
                     --model_config ${model}_config.yml
 done
