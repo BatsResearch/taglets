e82d030307654d6554a9366ee1382bb6c0945136
Cristina Menghini <cricri.menghini@gmail.com>
2023-03-22 16:30:43 -0400

    coop quantile

--- scripts/r_run_quantile_methods.sh
+++ scripts/r_run_quantile_methods.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=R-all_pseudo_meth
-#SBATCH --output=logs/resiscs45_all_pseudo_methods_split_500.out
+#SBATCH --job-name=R-quantile_meth
+#SBATCH --output=logs/resiscs45_quantile.out
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
@@ -19,7 +19,7 @@ sleep $[ ( $RANDOM % 30 )  + 1 ]s
 for vis_encoder in 'ViT-B/32'; do # 'ViT-B/32' 'RN50' 'ViT-L/14' 'RN101'
 for split_seed in 500; do #  0 200
 for dataset_name in RESICS45; do
-for model in quantile_vpt_pseudo_baseline; do #coop_pseudo_baseline vpt_pseudo_baseline; do #  all_vpt_pseudo_baseline; do 
+for model in quantile_coop_pseudo_baseline quantile_vpt_pseudo_baseline; do #coop_pseudo_baseline vpt_pseudo_baseline; do #  all_vpt_pseudo_baseline; do 
 for optim_seed in 1 2 3 4 5; do # 10 100 50 400 250; do
 
     export OPTIM_SEED="$optim_seed"
