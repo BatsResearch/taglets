8e05add0bce9f3dd267986f65dc3c6f5e1cbd75b
Cristina Menghini <cricri.menghini@gmail.com>
2023-03-23 18:59:16 -0400

    run combo and init in same script

--- scripts/r_run_init_methods.sh
+++ scripts/r_run_init_methods.sh
@@ -20,8 +20,8 @@ sleep $[ ( $RANDOM % 30 )  + 1 ]s
 for vis_encoder in 'ViT-B/32'; do # 'ViT-B/32' 'RN50' 'ViT-L/14' 'RN101'
 for split_seed in 500; do #  0 200
 for dataset_name in RESICS45; do
-for model in mix_vpt_baseline; do #  all_vpt_pseudo_baseline; do 
 for optim_seed in 1 2 3 4 5; do # 10 100 50 400 250; do
+for model in mix_vpt_baseline combo_vpt_baseline; do #  all_vpt_pseudo_baseline; do 
 for alpha in 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1; do
 
     export OPTIM_SEED="$optim_seed"
