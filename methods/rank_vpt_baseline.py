0f10174f9fa7a062e5c7631bd6a934bf21268959
Cristina Menghini <cricri.menghini@gmail.com>
2023-03-24 13:15:26 -0400

    only rank loss

--- methods/rank_vpt_baseline.py
+++ methods/rank_vpt_baseline.py
@@ -305,11 +305,11 @@ class RankVPTBaseline(InitVPTBaseline):
 
     def define_loss_function(self, logits, labs, logits_seen, teacher=False):
         
-        ce_loss = self.loss_ce(logits, labs)
+        #ce_loss = self.loss_ce(logits, labs)
         rank_loss = self.loss_rank(logits_seen, logits)
         log.info(f"Rank loss: {rank_loss}")
         log.info(f"CE loss: {ce_loss}")
-        loss_func = ce_loss + rank_loss
+        loss_func =  rank_loss # + ce_loss
         
         return loss_func
 
