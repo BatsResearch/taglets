ffcb0dcc5e1bab5b419dfb959d63d435c5e81936
Cristina Menghini <cricri.menghini@gmail.com>
2023-03-29 12:02:32 -0400

    mnist base

--- scripts/m_run_pseudo_methods.sh
+++ scripts/m_run_pseudo_methods.sh
@@ -2,9 +2,9 @@
 #SBATCH --job-name=M-pseudo_meth
 #SBATCH --output=logs/mnist_pseudo_methods.out
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=64G
-#SBATCH -t 48:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32G
+#SBATCH -t 96:00:00
 #SBATCH -p gpu --gres=gpu:4
 #SBATCH --exclude=gpu[717-718,1201-1204,1209,1403]
 
@@ -17,9 +17,9 @@ sleep $[ ( $RANDOM % 30 )  + 1 ]s
 
 
 for vis_encoder in 'ViT-B/32'; do # 'ViT-B/32' 'RN50' 'ViT-L/14' 'RN101'
-for split_seed in 500; do #  0 200
+for split_seed in 0 200; do #  0 200
 for dataset_name in MNIST; do
-for model in vpt_pseudo_baseline; do # coop_pseudo_baseline
+for model in coop_pseudo_baseline vpt_pseudo_baseline; do # coop_pseudo_baseline
 for optim_seed in 1 2 3 4 5; do
 
     export OPTIM_SEED="$optim_seed"
@@ -28,7 +28,7 @@ for optim_seed in 1 2 3 4 5; do
     export SPLIT_SEED="$split_seed"
     export MODEL="$model"
     
-    sed -i 's/^\(\s*main_process_port\s*:\s*\).*/\12076/'  accelerate_config.yml
+    sed -i 's/^\(\s*main_process_port\s*:\s*\).*/\12077/'  accelerate_config.yml
     accelerate launch --config_file ./accelerate_config.yml ./run_main.py \
                     --model_config ${model}_config.yml
 done
