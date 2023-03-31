3c0a8d20f3243f004fb595f5d3aeab4e257ed089
Cristina Menghini <cricri.menghini@gmail.com>
2023-03-31 12:56:43 -0400

    vpt

--- scripts/m_run_base_methods.sh
+++ scripts/m_run_base_methods.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
-#SBATCH -t 48:00:00
+#SBATCH -t 96:00:00
 #SBATCH -p gpu --gres=gpu:4
 ##SBATCH --exclude=gpu[717-718,1201-1204,1209,1403]
 
@@ -17,9 +17,9 @@ sleep $[ ( $RANDOM % 30 )  + 1 ]s
 
 
 for vis_encoder in 'ViT-B/32'; do # 'ViT-B/32' 'RN50' 'ViT-L/14' 'RN101'
-for split_seed in 0 200; do #  0 200
+for split_seed in 200; do #  0 200
 for dataset_name in MNIST; do
-for model in coop_baseline vpt_baseline; do # coop_baseline
+for model in vpt_baseline; do # coop_baseline
 for optim_seed in 1 2 3 4 5; do
 
     export OPTIM_SEED="$optim_seed"
