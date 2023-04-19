d6eb3298512e7ef2431934ebfe2d5a33090978b0
Cristina Menghini <cricri.menghini@gmail.com>
2023-04-19 00:45:40 -0400

    t,v,m prompts for FPL

--- scripts/semi_supervised_learning/run_visual_fpl.sh
+++ scripts/semi_supervised_learning/run_visual_fpl.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=R-base_meth
+#SBATCH --job-name=SSL-v
+#SBATCH --output=logs/ssl_visual_fpl.out
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=40G
@@ -19,7 +20,7 @@ for vis_encoder in 'ViT-B/32'; do # 'ViT-B/32'  'RN50' 'ViT-L/14' 'RN101'
 for split_seed in 500; do #  0 200
 for dataset_name in RESICS45; do
 for model in visual_fpl; do # coop_baseline
-for optim_seed in 1; do # 2 3 4 5; do #10 100 50 400 250; do
+for optim_seed in 1 2 3 4 5; do # 2 3 4 5; do #10 100 50 400 250; do
     
     export OPTIM_SEED="$optim_seed"
     export VIS_ENCODER="$vis_encoder"
@@ -33,7 +34,7 @@ for optim_seed in 1; do # 2 3 4 5; do #10 100 50 400 250; do
     # echo $TYPE
     # echo $PORT
     # $PORT
-    sed -i 's/^\(\s*main_process_port\s*:\s*\).*/\12073/'  accelerate_config.yml
+    sed -i 's/^\(\s*main_process_port\s*:\s*\).*/\12072/'  accelerate_config.yml
     accelerate launch --config_file ./accelerate_config.yml ./run_main_ssl.py \
                     --model_config ${model}_config.yml --learning_paradigm trzsl
 done
--- scripts/transductive_learning/run_textual_fpl.sh
+++ scripts/transductive_learning/run_textual_fpl.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=R-base_meth
+#SBATCH --job-name=TRZSL-t
+#SBATCH --output=logs/trzsl_textual_fpl.out
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=40G
@@ -19,7 +20,7 @@ for vis_encoder in 'ViT-B/32'; do # 'ViT-B/32'  'RN50' 'ViT-L/14' 'RN101'
 for split_seed in 500; do #  0 200
 for dataset_name in RESICS45; do
 for model in textual_fpl; do # coop_baseline
-for optim_seed in 1; do # 2 3 4 5; do #10 100 50 400 250; do
+for optim_seed in 1 2 3 4 5; do # 2 3 4 5; do #10 100 50 400 250; do
     
     export OPTIM_SEED="$optim_seed"
     export VIS_ENCODER="$vis_encoder"
