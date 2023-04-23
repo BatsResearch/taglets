933ef5a59670ca072a502aafada616ca34807afd
Cristina Menghini <cricri.menghini@gmail.com>
2023-04-23 10:19:29 -0400

    grip

--- scripts/DTD/UL/GRIP/run_grip_visual.sh
+++ scripts/DTD/UL/GRIP/run_grip_visual.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=v_grip_UL
+#SBATCH --job-name=DUL-vGRIP
+#SBATCH --output=logs/ul_dtd_image_grip.out
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=40G
@@ -14,28 +15,24 @@ source ../../zsl/bin/activate
 
 sleep $[ ( $RANDOM % 30 )  + 40 ]s
 
-
+for dataset_dir in '/users/cmenghin/data/bats/datasets/classification' ; do
 for vis_encoder in 'ViT-B/32'; do # 'ViT-B/32'  'RN50' 'ViT-L/14' 'RN101'
 for split_seed in 500; do #  0 200
-for dataset_name in RESICS45; do
+for dataset_name in DTD; do
 for model in grip_visual; do # coop_baseline
-for optim_seed in 1; do # 2 3 4 5; do #10 100 50 400 250; do
+for optim_seed in 1 2 3 4 5; do # 2 3 4 5; do #10 100 50 400 250; do
     
     export OPTIM_SEED="$optim_seed"
     export VIS_ENCODER="$vis_encoder"
     export DATASET_NAME="$dataset_name"
     export SPLIT_SEED="$split_seed"
     export MODEL="$model"
-    # export TYPE="$TYPE"
-    # export PREFIX_SIZE="$PREFIX_SIZE"
-    # #$PORT
-    # echo $optim_seed
-    # echo $TYPE
-    # echo $PORT
-    # $PORT
-    sed -i 's/^\(\s*main_process_port\s*:\s*\).*/\12076/'  accelerate_config.yml
+    export DATASET_DIR="$dataset_dir"
+    
+    sed -i 's/^\(\s*main_process_port\s*:\s*\).*/\12078/'  accelerate_config.yml
     accelerate launch --config_file ./accelerate_config.yml ./run_main_ul.py \
                     --model_config ${model}_config.yml --learning_paradigm ul
+done
 done
 done
 done
