7783abf83d27e958eb88030d4ea48a2dc74c5911
Cristina Menghini <cricri.menghini@gmail.com>
2023-05-03 12:11:02 +0200

    trzsl

--- scripts/RESICS45/TRZSL/BASE/run_base.sh
+++ scripts/RESICS45/TRZSL/BASE/run_base.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=RTR-BASE
-#SBATCH --output=logs/trzsl_resics_base_vit.out
+#SBATCH --output=logs/trzsl_resics_base_split_2.out
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=40G
-#SBATCH -t 48:00:00
+#SBATCH -t 72:00:00
 #SBATCH -p gpu --gres=gpu:4
 ##SBATCH --exclude=gpu[717-718,1201-1204,1209,1403,2103]
 
@@ -16,8 +16,8 @@ source ../../zsl/bin/activate
 sleep $[ ( $RANDOM % 30 )  + 40 ]s
 
 for dataset_dir in '/users/cmenghin/data/bats/datasets/classification' ; do
-for vis_encoder in 'ViT-L/14'; do # 'ViT-B/32'  'RN50' 'ViT-L/14' 'RN101'
-for split_seed in 500; do #  0 200
+for vis_encoder in 'ViT-B/32' 'ViT-L/14'; do # 'ViT-B/32'  'RN50' 'ViT-L/14' 'RN101'
+for split_seed in 0; do # 500 0 200
 for dataset_name in RESICS45; do
 for model in textual_prompt visual_prompt multimodal_prompt; do # 
 for optim_seed in 1 2 3 4 5; do # 2 3 4 5; do #10 100 50 400 250; do
@@ -29,7 +29,7 @@ for optim_seed in 1 2 3 4 5; do # 2 3 4 5; do #10 100 50 400 250; do
     export MODEL="$model"
     export DATASET_DIR="$dataset_dir"
 
-    sed -i 's/^\(\s*main_process_port\s*:\s*\).*/\12076/'  accelerate_config.yml
+    sed -i 's/^\(\s*main_process_port\s*:\s*\).*/\12077/'  accelerate_config.yml
     accelerate launch --config_file ./accelerate_config.yml ./run_main_trzsl.py \
                     --model_config ${model}_config.yml --learning_paradigm trzsl
 done
