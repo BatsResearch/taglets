c97174005db8e7c5f0bc0017c1cbc592d645bd28
Cristina Menghini <cricri.menghini@gmail.com>
2023-05-04 09:11:36 +0200

    il fgvca

--- scripts/FGVCAircraft/UL/GRIP/run_grip_visual.sh
+++ scripts/FGVCAircraft/UL/GRIP/run_grip_visual.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=v_grip_UL
+#SBATCH --job-name=AUL-vGRIP
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=40G
-#SBATCH -t 48:00:00
+#SBATCH -t 96:00:00
 #SBATCH -p gpu --gres=gpu:4
 ##SBATCH --exclude=gpu[717-718,1201-1204,1209,1403]
 
@@ -14,10 +14,10 @@ source ../../zsl/bin/activate
 
 sleep $[ ( $RANDOM % 30 )  + 40 ]s
 
-
+for dataset_dir in '/users/cmenghin/data/bats/datasets/classification' ; do
 for vis_encoder in 'ViT-B/32'; do # 'ViT-B/32'  'RN50' 'ViT-L/14' 'RN101'
 for split_seed in 500; do #  0 200
-for dataset_name in RESICS45; do
+for dataset_name in FGVCAircraft; do
 for model in grip_visual; do # coop_baseline
 for optim_seed in 1; do # 2 3 4 5; do #10 100 50 400 250; do
     
@@ -26,16 +26,12 @@ for optim_seed in 1; do # 2 3 4 5; do #10 100 50 400 250; do
     export DATASET_NAME="$dataset_name"
     export SPLIT_SEED="$split_seed"
     export MODEL="$model"
-    # export TYPE="$TYPE"
-    # export PREFIX_SIZE="$PREFIX_SIZE"
-    # #$PORT
-    # echo $optim_seed
-    # echo $TYPE
-    # echo $PORT
-    # $PORT
+    export DATASET_DIR="$dataset_dir"
+    
     sed -i 's/^\(\s*main_process_port\s*:\s*\).*/\12076/'  accelerate_config.yml
     accelerate launch --config_file ./accelerate_config.yml ./run_main_ul.py \
                     --model_config ${model}_config.yml --learning_paradigm ul
+done
 done
 done
 done
