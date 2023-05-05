535e1ce879461ccb4b60031daf6a15016eca3066
Cristina Menghini <cricri.menghini@gmail.com>
2023-05-05 09:39:41 -0400

    grip

--- scripts/EuroSAT/UL/GRIP/run_grip_textual.sh
+++ scripts/EuroSAT/UL/GRIP/run_grip_textual.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=EUL-tGRIP
-#SBATCH --output=logs/ul_eurosat_text_grip.out
+#SBATCH --output=logs/ul_eurosat_text_grip_vit.out
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=40G
-#SBATCH -t 48:00:00
+#SBATCH -t 96:00:00
 #SBATCH -p gpu --gres=gpu:4
 ##SBATCH --exclude=gpu[717-718,1201-1204,1209,1403]
 
@@ -16,11 +16,11 @@ source ../../zsl/bin/activate
 sleep $[ ( $RANDOM % 30 )  + 40 ]s
 
 for dataset_dir in '/users/cmenghin/data/bats/datasets/classification' ; do
-for vis_encoder in 'ViT-B/32'; do # 'ViT-B/32'  'RN50' 'ViT-L/14' 'RN101'
+for vis_encoder in 'ViT-L/14'; do # 'ViT-B/32'  'RN50' 'ViT-L/14' 'RN101'
 for split_seed in 500; do #  0 200
-for dataset_name in RESICS45; do
+for dataset_name in EuroSAT; do
 for model in grip_textual; do # coop_baseline
-for optim_seed in 1; do # 2 3 4 5; do #10 100 50 400 250; do
+for optim_seed in 1 2 3 4 5; do # 2 3 4 5; do #10 100 50 400 250; do
     
     export OPTIM_SEED="$optim_seed"
     export VIS_ENCODER="$vis_encoder"
@@ -29,7 +29,7 @@ for optim_seed in 1; do # 2 3 4 5; do #10 100 50 400 250; do
     export MODEL="$model"
     export DATASET_DIR="$dataset_dir"
 
-    sed -i 's/^\(\s*main_process_port\s*:\s*\).*/\12073/'  accelerate_config.yml
+    sed -i 's/^\(\s*main_process_port\s*:\s*\).*/\12072/'  accelerate_config.yml
     accelerate launch --config_file ./accelerate_config.yml ./run_main_ul.py \
                     --model_config ${model}_config.yml --learning_paradigm ul
 done
