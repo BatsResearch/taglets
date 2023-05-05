8952ece496be8505287b78740fcd6aa95e1a7084
Cristina Menghini <cricri.menghini@gmail.com>
2023-05-04 21:43:20 -0400

    grips

--- scripts/FGVCAircraft/UL/GRIP/run_grip_textual.sh
+++ scripts/FGVCAircraft/UL/GRIP/run_grip_textual.sh
@@ -20,7 +20,7 @@ for vis_encoder in 'ViT-B/32'; do # 'ViT-B/32'  'RN50' 'ViT-L/14' 'RN101'
 for split_seed in 500; do #  0 200
 for dataset_name in FGVCAircraft; do
 for model in grip_textual; do # coop_baseline
-for optim_seed in 1; do # 2 3 4 5; do #10 100 50 400 250; do
+for optim_seed in 2 3 4 5; do # 2 3 4 5; do #10 100 50 400 250; do
     
     export OPTIM_SEED="$optim_seed"
     export VIS_ENCODER="$vis_encoder"
--- scripts/Flowers102/TRZSL/GRIP/run_grip_textual.sh
+++ scripts/Flowers102/TRZSL/GRIP/run_grip_textual.sh
@@ -16,7 +16,7 @@ source ../../zsl/bin/activate
 sleep $[ ( $RANDOM % 30 )  + 40 ]s
 
 for dataset_dir in '/users/cmenghin/data/bats/datasets/classification' ; do
-for vis_encoder in 'ViT-B/32'; do # 'ViT-B/32'  'RN50' 'ViT-L/14' 'RN101'
+for vis_encoder in 'ViT-L/14'; do # 'ViT-B/32'  'RN50' 'ViT-L/14' 'RN101'
 for split_seed in 500; do #  0 200
 for dataset_name in Flowers102; do
 for model in grip_textual; do # coop_baseline
