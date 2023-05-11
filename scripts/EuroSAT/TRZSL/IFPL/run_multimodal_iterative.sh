e183bbd49838e924e630963cf0e092d014b6b7c7
Cristina Menghini <cricri.menghini@gmail.com>
2023-05-11 08:02:16 -0400

    reza jobs

--- scripts/EuroSAT/TRZSL/IFPL/run_multimodal_iterative.sh
+++ scripts/EuroSAT/TRZSL/IFPL/run_multimodal_iterative.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=R-base_meth
+#SBATCH --job-name=ETR-mIFPL
+#SBATCH --output=logs/trzsl_eurosat_multi_ifpl.out
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=40G
-#SBATCH -t 48:00:00
+#SBATCH -t 96:00:00
 #SBATCH -p gpu --gres=gpu:4
 ##SBATCH --exclude=gpu[717-718,1201-1204,1209,1403]
 
@@ -14,28 +15,24 @@ source ../../zsl/bin/activate
 
 sleep $[ ( $RANDOM % 30 )  + 40 ]s
 
-
+for dataset_dir in '/users/cmenghin/data/bats/datasets/classification' ; do
 for vis_encoder in 'ViT-B/32'; do # 'ViT-B/32'  'RN50' 'ViT-L/14' 'RN101'
 for split_seed in 500; do #  0 200
-for dataset_name in RESICS45; do
+for dataset_name in EuroSAT; do
 for model in iterative_multimodal_fpl; do # coop_baseline
-for optim_seed in 1; do # 2 3 4 5; do #10 100 50 400 250; do
+for optim_seed in 1 2 3 4 5; do # 2 3 4 5; do #10 100 50 400 250; do
     
     export OPTIM_SEED="$optim_seed"
     export VIS_ENCODER="$vis_encoder"
     export DATASET_NAME="$dataset_name"
     export SPLIT_SEED="$split_seed"
     export MODEL="$model"
-    # export TYPE="$TYPE"
-    # export PREFIX_SIZE="$PREFIX_SIZE"
-    # #$PORT
-    # echo $optim_seed
-    # echo $TYPE
-    # echo $PORT
-    # $PORT
-    sed -i 's/^\(\s*main_process_port\s*:\s*\).*/\12071/'  accelerate_config.yml
+    export DATASET_DIR="$dataset_dir"
+
+    sed -i 's/^\(\s*main_process_port\s*:\s*\).*/\12070/'  accelerate_config.yml
     accelerate launch --config_file ./accelerate_config.yml ./run_main_trzsl.py \
                     --model_config ${model}_config.yml --learning_paradigm trzsl
+done
 done
 done
 done
