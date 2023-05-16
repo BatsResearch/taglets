7391889afcc7d602e0bb57cd63df23f025147bb1
Cristina Menghini <cricri.menghini@gmail.com>
2023-05-15 21:10:07 -0400

    trzsl grip flo

--- scripts/Flowers102/TRZSL/GRIP/run_grip_textual.sh
+++ scripts/Flowers102/TRZSL/GRIP/run_grip_textual.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=FTR-tGRIP
-#SBATCH --output=logs/trzsl_flower_text_grip.out
+#SBATCH --output=logs/trzsl_flower_text_grip_split_2.out
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=40G
 #SBATCH -t 48:00:00
 #SBATCH -p gpu --gres=gpu:4
 ##SBATCH --exclude=gpu[717-718,1201-1204,1209,1403]
+#SBATCH --partition=3090-gcondo
 
 module load cuda/11.1.1
 
@@ -16,8 +17,8 @@ source ../../zsl/bin/activate
 sleep $[ ( $RANDOM % 30 )  + 40 ]s
 
 for dataset_dir in '/users/cmenghin/data/bats/datasets/classification' ; do
-for vis_encoder in 'ViT-L/14'; do # 'ViT-B/32'  'RN50' 'ViT-L/14' 'RN101'
-for split_seed in 500; do #  0 200
+for vis_encoder in 'ViT-B/32'; do # 'ViT-B/32'  'RN50' 'ViT-L/14' 'RN101'
+for split_seed in 0; do #  0 200
 for dataset_name in Flowers102; do
 for model in grip_textual; do # coop_baseline
 for optim_seed in 1 2 3 4 5; do # 2 3 4 5; do #10 100 50 400 250; do
@@ -29,7 +30,7 @@ for optim_seed in 1 2 3 4 5; do # 2 3 4 5; do #10 100 50 400 250; do
     export MODEL="$model"
     export DATASET_DIR="$dataset_dir"
     
-    sed -i 's/^\(\s*main_process_port\s*:\s*\).*/\12073/'  accelerate_config.yml
+    sed -i 's/^\(\s*main_process_port\s*:\s*\).*/\12072/'  accelerate_config.yml
     accelerate launch --config_file ./accelerate_config.yml ./run_main_trzsl.py \
                     --model_config ${model}_config.yml --learning_paradigm trzsl
 done
