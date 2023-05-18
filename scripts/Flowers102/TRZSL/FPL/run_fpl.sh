d3070c285d7a47a79f7dc21dc2365dfe7d58aec4
Cristina Menghini <cricri.menghini@gmail.com>
2023-05-18 00:47:23 -0400

    flo fpl

--- scripts/Flowers102/TRZSL/FPL/run_fpl.sh
+++ scripts/Flowers102/TRZSL/FPL/run_fpl.sh
@@ -7,7 +7,7 @@
 #SBATCH -t 48:00:00
 #SBATCH -p gpu --gres=gpu:4
 ##SBATCH --exclude=gpu[717-718,1201-1204,1209,1403]
-#SBATCH --partition=3090-gcondo
+##SBATCH --partition=3090-gcondo
 
 module load cuda/11.1.1
 
@@ -18,7 +18,7 @@ sleep $[ ( $RANDOM % 30 )  + 40 ]s
 
 for dataset_dir in '/users/cmenghin/data/bats/datasets/classification' ; do
 for vis_encoder in 'ViT-B/32'; do # 'ViT-B/32'  'RN50' 'ViT-L/14' 'RN101'
-for split_seed in 0; do #  0 200
+for split_seed in 200; do #  0 200
 for dataset_name in Flowers102; do
 for model in textual_fpl visual_fpl multimodal_fpl; do # 
 for optim_seed in 1 2 3 4 5; do # 2 3 4 5; do #10 100 50 400 250; do
@@ -30,7 +30,7 @@ for optim_seed in 1 2 3 4 5; do # 2 3 4 5; do #10 100 50 400 250; do
     export MODEL="$model"
     export DATASET_DIR="$dataset_dir"
 
-    sed -i 's/^\(\s*main_process_port\s*:\s*\).*/\12078/'  accelerate_config.yml
+    sed -i 's/^\(\s*main_process_port\s*:\s*\).*/\12075/'  accelerate_config.yml
     accelerate launch --config_file ./accelerate_config.yml ./run_main_trzsl.py \
                     --model_config ${model}_config.yml --learning_paradigm trzsl
 done
