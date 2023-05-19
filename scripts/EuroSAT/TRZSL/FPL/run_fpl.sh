f92c734fce80f2bc10191309a63c38511f4875bc
Cristina Menghini <cricri.menghini@gmail.com>
2023-05-18 22:14:41 -0400

    trzsl

--- scripts/EuroSAT/TRZSL/FPL/run_fpl.sh
+++ scripts/EuroSAT/TRZSL/FPL/run_fpl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=ETR-FPL
-#SBATCH --output=logs/trzsl_eurosat_fpl_split_2.out
+#SBATCH --output=logs/trzsl_eurosat_fpl_split_3.out
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=40G
@@ -18,7 +18,7 @@ sleep $[ ( $RANDOM % 30 )  + 40 ]s
 
 for dataset_dir in '/users/cmenghin/data/bats/datasets/classification' ; do
 for vis_encoder in 'ViT-B/32'; do # 'ViT-B/32'  'RN50' 'ViT-L/14' 'RN101'
-for split_seed in 0; do #  0 200
+for split_seed in 200; do #  0 200
 for dataset_name in EuroSAT; do
 for model in textual_fpl visual_fpl multimodal_fpl; do # coop_baseline
 for optim_seed in 1 2 3 4 5; do # 2 3 4 5; do #10 100 50 400 250; do
@@ -30,7 +30,7 @@ for optim_seed in 1 2 3 4 5; do # 2 3 4 5; do #10 100 50 400 250; do
     export MODEL="$model"
     export DATASET_DIR="$dataset_dir"
     
-    sed -i 's/^\(\s*main_process_port\s*:\s*\).*/\12072/'  accelerate_config.yml
+    sed -i 's/^\(\s*main_process_port\s*:\s*\).*/\12071/'  accelerate_config.yml
     accelerate launch --config_file ./accelerate_config.yml ./run_main_trzsl.py \
                     --model_config ${model}_config.yml --learning_paradigm trzsl
 done
