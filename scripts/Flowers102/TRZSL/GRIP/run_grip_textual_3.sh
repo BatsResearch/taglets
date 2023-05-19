67c0d969adc7d92a6bd468b316f62cbe574d282a
Cristina Menghini <cricri.menghini@gmail.com>
2023-05-19 08:06:26 -0400

    fgvcs splt 3

---
+++ scripts/Flowers102/TRZSL/GRIP/run_grip_textual_3.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH --job-name=3-ATR-tGRIP
+#SBATCH --output=logs/trzsl_fgvca_text_grip_split_2_seed_3.out
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=40G
+#SBATCH -t 72:00:00
+#SBATCH -p gpu --gres=gpu:4
+##SBATCH --exclude=gpu[717-718,1201-1204,1209,1403]
+#SBATCH --partition=3090-gcondo
+
+module load cuda/11.1.1
+
+pwd
+source ../../zsl/bin/activate
+
+sleep $[ ( $RANDOM % 30 )  + 40 ]s
+
+for dataset_dir in '/users/cmenghin/data/bats/datasets/classification' ; do
+for vis_encoder in 'ViT-B/32'; do # 'ViT-B/32'  'RN50' 'ViT-L/14' 'RN101'
+for split_seed in 0; do #  0 200
+for dataset_name in FGVCAircraft; do
+for model in grip_textual; do # coop_baseline
+for optim_seed in 3; do # 2 3 4 5; do #10 100 50 400 250; do
+    
+    export OPTIM_SEED="$optim_seed"
+    export VIS_ENCODER="$vis_encoder"
+    export DATASET_NAME="$dataset_name"
+    export SPLIT_SEED="$split_seed"
+    export MODEL="$model"
+    export DATASET_DIR="$dataset_dir"
+
+    sed -i 's/^\(\s*main_process_port\s*:\s*\).*/\12071/'  accelerate_config.yml
+    accelerate launch --config_file ./accelerate_config.yml ./run_main_trzsl.py \
+                    --model_config ${model}_config.yml --learning_paradigm trzsl
+done
+done
+done
+done
+done
+done
